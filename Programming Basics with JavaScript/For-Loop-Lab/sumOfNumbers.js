00b47d9429b17c8f7da0b9edf54944aaaaa73f9d
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-01-09 13:01:17 -0800

    sumOfNumbers.js
    Program calculates the sum of digits provided from the source.

---
+++ Programming Basics with JavaScript/For-Loop-Lab/sumOfNumbers.js
@@ -0,0 +1,14 @@
+function sumOfNumbers(input) {
+    let num = input[0];
+    let sum = 0;
+
+    for(let i = 0; i < num.length; i++){
+        sum += Number(num[i]);
+    }
+
+    console.log(`The sum of the digits is:${sum}`)
+}
+
+sumOfNumbers((["1234"]))
+
+sumOfNumbers(["564891"])
