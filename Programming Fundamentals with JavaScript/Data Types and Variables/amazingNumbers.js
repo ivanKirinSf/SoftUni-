1dcba2496b58c4e42fd71c44f48f504d274d0a35
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-01-28 12:52:05 -0800

    amazingNumbers.js
    Program will receive a number.
    Check and print if it is amazing or not into the following format: 
    	"{number} Amazing? {True or False}"
    An amazing number includes the digit 9 the sum of its digits. 
    Examples for amazing numbers are 1233 (1 + 2 + 3 + 3 = 9), 583472 (5 + 8 + 3 + 4 + 7 + 2 = 29)
    Examples
    Input	Output
    1233	1233 Amazing? True
    999	999 Amazing? False

---
+++ Programming Fundamentals with JavaScript/Data Types and Variables/amazingNumbers.js
@@ -0,0 +1,16 @@
+function solve(num){
+    num = num.toString();
+    let sum = 0;
+    for(let i = 0; i < num.length; i++){
+        sum += Number(num[i]);
+    }
+
+    let res = sum.toString().includes('9');
+
+   
+
+   console.log(res ? `${num} Amazing? True` : `${num} Amazing? False`)
+}
+
+solve(1233)
+solve(999)
