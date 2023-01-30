8a9252e98353bd12b2b99564205db8e806a58c7a
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-01-30 11:08:11 -0800

    requiredReading.js
    Write a function to help Ivan calculate how many hours a day he has to spend reading the necessary literature from the list given for the summer vacation.
    As input, you will receive 3 parameters: 
    •	Number of pages of the current book - integer [1… 1000] 
    •	Pages read in 1 hour - integer [1… 1000]
    •	The number of days for which you must read the book -  integer [1… 1000]
    As output print on the console the number of hours, that Ivan has to read each day.

---
+++ Programming Fundamentals with JavaScript/Data Types and Variables/requiredReading.js
@@ -0,0 +1,14 @@
+function solve(bookPages, pagesPerHour, days){
+    let hoursPerDay = 0;
+    let time = 0;
+
+    time = bookPages/pagesPerHour;
+    hoursPerDay = time/days;
+
+    console.log(hoursPerDay)
+
+}
+
+solve(212,20 ,2)
+
+solve(432,15,4 )
