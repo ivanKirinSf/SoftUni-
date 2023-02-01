dcfa55a979a1bd6588b2bf4d91f0e527b0027f32
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-01 10:52:18 -0800

    lowerOrUpper.js
    Write a function that prints whether a given character is upper-case or lower-case.

---
+++ Programming Fundamentals with JavaScript/Data Types and Variables - Exercise/lowerOrUpper.js
@@ -0,0 +1,10 @@
+function lowerOrUpper(char){
+    if(char.charCodeAt(0) >= 65 && char.charCodeAt(0)<=90){
+        console.log("upper-case")
+    } else {
+        console.log("lower-case")
+    }
+
+}
+
+lowerOrUpper('L')
