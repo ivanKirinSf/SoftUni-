41b226c2a19d9ff4553378790cf09f5ce5516266
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-01 09:44:25 -0800

    sumDigits.js
    Function, will be given a single number. Its task is to find the sum of its digits.

---
+++ Programming Fundamentals with JavaScript/Data Types and Variables - Exercise/sumDigits.js
@@ -0,0 +1,10 @@
+function sumDigits(num){
+    let numToString= num.toString(); 
+
+    let sum = 0;
+    
+    for(let i=0; i<numToString.length;i++){
+                sum+= Number(numToString[i]);
+    }
+    console.log(sum)
+}
