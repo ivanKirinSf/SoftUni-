e37da16d3d736b522364ffc1cd3f75bac3534571
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-03 09:32:29 -0800

    sumFirstAndLastElements.js
    Write a function that receives an array of numbers and prints the sum of the first and last element in that array.

---
+++ Programming Fundamentals with JavaScript/Arrays/sumFirstAndLastElements.js
@@ -0,0 +1,14 @@
+function sumFirstAndLastElements(arr){
+    let firstElement = arr[0];
+    let lastIndex = arr.length-1;
+    let lastElement = arr[lastIndex];
+    console.log(firstElement + lastElement);
+    
+
+
+}
+
+
+let myArr = [20,30,40];
+sumFirstAndLast(myArr);
+sumFirstAndLast([10, 17, 22, 33])
