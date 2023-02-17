7ee5dc81551e1293d6ab993c423bd1fb18c5ccc1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-17 03:31:15 -0800

    Update maxSequence.js

--- Programming Fundamentals with JavaScript/Arrays - Exercise/maxSequence.js
+++ Programming Fundamentals with JavaScript/Arrays - Exercise/maxSequence.js
@@ -1,61 +1,81 @@
 function maxSequence(arr) {
 
-  let newArr = [];
-  let maxArr = [];
-  let leftArr = [];
-  let length = 0;
-  let maxLength = 1;
+    let newArr = [];
+    let maxArr = [];
+    let leftArr = [];
+    let length = 0;
+    let maxLength = 1;
 
-  for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
-    let tempI = arr[i];
-
-
-    for (let k = i + 1; k < arr.length; k++) {
-      let tempK = arr[k]
-
-      if (tempI == tempK) {
-        newArr.push(tempK);
-        length = newArr.length;
-        if (length > maxLength) {
-          maxArr = newArr;
-          if (length < arr.length) {
-            maxArr.push(tempI);
-          } else {
-            break;
-          }
+        let tempI = arr[i];
 
 
+        for (let k = i + 1; k < arr.length; k++) {
+            let tempK = arr[k]
+
+            if (tempI == tempK) {
+                if (maxArr.length < arr.length) {
+                    newArr.push(tempK);
+                    if(newArr.length > leftArr.length){
+                        leftArr.push(tempK);
+                        leftArr.push(tempI);
+                        }
+                    //if(tempK != arr[k+1]){
+                    //    break;
+                    //}
+                }
+                if (maxArr.length >= arr.Length) {
+                    break;
+                }
+                length = newArr.length;
+                if (length > maxLength) {   //(length > maxLength) 
+                    maxArr = newArr;
+                    if (length < arr.length){ //&& tempK == arr[k+1] ) {
+                       maxArr.push(tempI)
+                    } else {
+                        maxLength = length;
+                        break;
+                    }
+
+                }
+
+
+
+                
+                
+                
+
+
+
+
+            } else {
+                newArr = [];
+                break;
+            }
+
+
+
+
+            maxLength = length;
 
         }
-        
-        
-        
-
-      } else {
-        newArr = [];
-        break;
-      }
 
 
 
 
-      maxLength = length;
+
+
+
 
     }
 
-    
+    if(leftArr.length > maxArr.length){
+        console.log(leftArr.join(" "))
+    } else {
+    console.log(maxArr.join(" "))
+    }
 
-
-
-
-
-
-  }
-
-
-  console.log(maxArr)
-  
 
 
 }
