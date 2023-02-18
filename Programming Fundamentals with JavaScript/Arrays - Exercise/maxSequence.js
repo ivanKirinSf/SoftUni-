7523ebd2e8b026611d9ccb52f69630be0ef9241b
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-18 05:43:20 -0800

    maxSequence.js
    Write a function that finds the longest sequence of equal elements in an array of numbers. 
    If several longest sequences exist, print the leftmost one.
    Examples
    Input	Output
    [2, 1, 1, 2, 3, 3, 2, 2, 2, 1]	2 2 2
    [1, 1, 1, 2, 3, 1, 3, 3]	1 1 1
    [4, 4, 4, 4]	4 4 4 4
    [0, 1, 1, 5, 2, 2, 6, 3, 3]	1 1

--- Programming Fundamentals with JavaScript/Arrays - Exercise/maxSequence.js
+++ Programming Fundamentals with JavaScript/Arrays - Exercise/maxSequence.js
@@ -1,83 +1,34 @@
 function maxSequence(arr) {
-
     let newArr = [];
     let maxArr = [];
-    let leftArr = [];
-    let length = 0;
-    let maxLength = 1;
-
-    for (let i = 0; i < arr.length; i++) {
-
-        let tempI = arr[i];
-
-
-        for (let k = i + 1; k < arr.length; k++) {
-            let tempK = arr[k]
-
-            if (tempI == tempK) {
-                if (maxArr.length < arr.length) {
-                    newArr.push(tempK);
-                    if(newArr.length > leftArr.length){
-                        leftArr.push(tempK);
-                        leftArr.push(tempI);
-                        }
-                    //if(tempK != arr[k+1]){
-                    //    break;
-                    //}
-                }
-                if (maxArr.length >= arr.Length) {
-                    break;
-                }
-                length = newArr.length;
-                if (length > maxLength) {   //(length > maxLength) 
-                    maxArr = newArr;
-                    if (length < arr.length){ //&& tempK == arr[k+1] ) {
-                       maxArr.push(tempI)
-                    } else {
-                        maxLength = length;
-                        break;
-                    }
-
-                }
-
-
-
-                
-                
-                
-
-
-
-
-            } else {
-                newArr = [];
-                break;
-            }
-
-
-
-
-            maxLength = length;
-
-        }
-
-
-
-
-
-
-
-
+    
+  
+    for(let i = 0; i<arr.length; i++){
+      let tempI = arr[i];
+      
+      
+      index = i;
+      while(tempI === arr[index]){
+        newArr.push(tempI);
+        index++
+        
+      }
+  
+  
+      if(newArr.length > maxArr.length){
+      maxArr=newArr
+      }
+  
+      newArr = [];
+  
+      
+      
+  
+      
     }
-
-    if(leftArr.length > maxArr.length){
-        console.log(leftArr.join(" "))
-    } else {
+  
     console.log(maxArr.join(" "))
-    }
+  
+  }
 
-
-
-}
-
-maxSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
+maxSequence([0, 1, 1, 5, 2, 2, 6, 3, 3])
