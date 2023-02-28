958d0816afb3a15121be7c953b6970a7bb0c0c64
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-02-28 01:29:28 -0800

    Update equalSums.js

--- Programming Fundamentals with JavaScript/Arrays - Exercise/equalSums.js
+++ Programming Fundamentals with JavaScript/Arrays - Exercise/equalSums.js
@@ -23,3 +23,45 @@ function equalSums(arr) {
         if (i == arr.length - 2) {
             sumRight = arr[arr.length - 1]
         }
+        
+        if(i < arr.length-2){
+            sumRight = 0;
+        for (let r = i + 1; r < arr.length; r++) {
+            let tempR = arr[r];
+            sumRight += tempR;
+        }
+    }
+
+        if (i > 1) {
+            sumLeft = 0;
+            for (let l = i - 1; l >= 0; l--) {
+                let tempL = arr[l];
+                sumLeft += tempL
+
+            }
+        }
+
+
+        if(sumLeft == sumRight){
+            equal = true;
+            index = i;
+        } 
+
+
+    }
+
+    if(equal == true){
+        console.log(index)
+    } else {
+        console.log("no")
+    }
+
+   
+
+
+
+
+
+}
+
+equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4])
