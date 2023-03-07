c9c49eba5ee9da2ae3388583901afbead0fa5bc9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-03-07 10:34:30 -0800

    dungeonestDark02.js
    You have initial health 100 and initial coins 0. You will be given a string, representing the dungeon's rooms. Each room is separated with '|' (vertical bar): "room1|room2|room3…"
    Each room contains - an item or a monster; and a number. They are separated by a single space.
     ("item/monster number").
    •	If the first part is "potion": 
    o	You are healed with the number in the second part. However, your health cannot exceed your initial health (100). 
    o	Print: `You healed for {healing-number} hp.`
    o	After that, print your current health: `Current health: {number} hp.`
    •	If the first part is "chest": 
    o	You have found some coins, the number in the second part. 
    o	Print: `You found {coins} coins.`
    •	In any other case, you are facing a monster, you are going to fight. 
    The second part of the room contains the attack of the monster, and the first the monster's name. You should remove the monster's attack from your health. 
    o	If you are not dead (health > 0) you have slain the monster, and you should print: 
                                       `You slayed {monster-name}.`
    o	If you have died, print: `You died! Killed by {monster-name}.` and your quest is over. 
    Print the best room you`ve to manage to reach: `Best room: {room}`.
    •	If you managed to go through all the rooms in the dungeon, print on the next three lines: 
    "You've made it!"
     "Coins: {coins}"
     "Health: {health}"
    Input
    You receive an array with one element- string, representing the dungeon's rooms, separated with '|' (vertical bar): ["room1|room2|room3…"].
    Output
    Print the corresponding messages, described above.
    
    input: ["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]   
    
    output: You slayed rat.
    You slayed bat.
    You healed for 10 hp.
    Current health: 80 hp.
    You slayed rat.
    You found 100 coins.
    You died! Killed by boss.
    Best room: 6
    
    input: ["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]   
    
    output: You slayed cat.
    You healed for 10 hp.
    Current health: 100 hp.
    You slayed orc.
    You found 10 coins.
    You slayed snake.
    You found 110 coins.
    You've made it!
    Coins: 120
    Health: 65

--- Programming Fundamentals with JavaScript/Arrays - Exercise/dungeonestDark02.js
+++ Programming Fundamentals with JavaScript/Arrays - Exercise/dungeonestDark02.js
@@ -1,78 +1,58 @@
-function dungeonestDark (input){
+function dungeonestDark(input){
 
     let text = input[0];
     let rooms = text.split("|");
     let health = 100;
     let coins = 0;
-    let roomNum = 0;
 
     for(let i = 0; i<rooms.length; i++){
-        let room = rooms[i];
-        roomNum ++;
+        let temp = rooms[i];
+        let room = temp.split(" ")
+        let item = room[0];
+        let num = Number(room[1]);
 
+        if(item === "potion"){
+            health += num;
+            if(health <= 100){
+            console.log(`You healed for ${num} hp.`);
+            console.log(`Current health: ${health} hp.`);
+            } else if (health > 100){
+                let tempHealth = num - (health - 100);
+                health = 100;
+                console.log(`You healed for ${tempHealth} hp.`);
+                console.log(`Current health: ${health} hp.`);
 
-        let commands = room.split(" ");
-        let comand = commands[0];
-        let volume = Number(commands[1]);
+            }
 
-        switch(comand){
-            case "potion": 
-                let tempHealth = health;
-                health += volume;
-                if(health <= 100){
-                console.log(`You healed for ${volume} hp.`)
-                console.log(`Current health: ${health} hp.`)
-            } else{
-                let extra = health - 100;
-                let temp = volume - extra;
-                
-                tempHealth += temp;
-                health = tempHealth;
-                console.log(`You healed for ${temp} hp.`)
-                console.log(`Current health: 100 hp.`)
-                
-            };
-            break;
-            case "chest": coins += volume;
-            if(volume>0 && health>0){
-            console.log(`You found ${volume} coins.`);
-            };
-            break;
-
-            default : health -= volume;
+        } else if( item === "chest"){
+            coins += num;
+            console.log(`You found ${num} coins.`)
+        }else{
+            health -= num;
             if(health > 0){
-                console.log(`You slayed ${comand}.`);
-            } else {
-                console.log(`You died! Killed by ${comand}.`)
-                console.log(`Best room: ${roomNum}`);
+                console.log(`You slayed ${item}.`);
+
+            } else if(health <= 0){
+                console.log(`You died! Killed by ${item}.`);
+                console.log(`Best room: ${i+1}`);
                 break;
-            }break;
-            
-
-
-            
+            }
         }
-
-        if(health > 0 && roomNum == rooms.length){   //if(health > 0 && roomNum == rooms.length){
-            console.log("You've made it!")
-            console.log(`Coins: ${coins}`)
-            console.log(`Health: ${health}`);
-            break;
-        
-
-        }
-
-
 
         
     }
-    
+
+    if(health > 0){
+        console.log("You've made it!");
+        console.log(`Coins: ${coins}`);
+        console.log(`Health: ${health}`);
+
+    }
 
 
 
-    
 
 }
 
-dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
 dungeonestDark(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"])
+dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
