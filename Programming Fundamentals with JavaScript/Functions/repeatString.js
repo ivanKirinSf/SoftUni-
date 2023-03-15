ca538fe6dc66bb032d56fa251b5f13b9cfff5166
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-03-15 02:24:13 -0700

    repeatString.js
    Write a function that receives a string and a repeat count n. The function should return a new string (the old one repeated n times).
    
    Input: "abc", 3    Output: abcabcabc
    
    Input: "String", 2    Output: StringString

---
+++ Programming Fundamentals with JavaScript/Functions/repeatString.js
@@ -0,0 +1,11 @@
+function repeatString(str, repeatNum){
+
+    let result = copyRepeatString(str, repeatNum);
+    return result; 
+
+    function copyRepeatString(str, repeatNum){
+        return str.repeat(repeatNum);
+    }
+
+
+}
