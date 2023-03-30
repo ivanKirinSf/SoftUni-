05b83ff37bb885342e376ccba899bb5d0a748210
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-03-30 02:05:03 -0700

    addAndSubtract3.js
    You will receive three integer numbers. 
    Write a function sum() to calculate the sum of the first two integers and a function subtract(), which subtracts the result of the function the sum() and the third integer.
    
    Input: 23, 6, 10    Output: 19
    Input: 1, 17, 30    Output: -12
    Input: 42,    Output: 0

---
+++ Programming Fundamentals with JavaScript/Functions - Exercise/addAndSubtract3.js
@@ -0,0 +1,17 @@
+function addAndSubtract(a, b, c){
+
+  let sum = sumTwoNumbers(a, b);
+  let subtract = subtractTwoNumbers(sum, c);
+
+  return subtract;
+
+function sumTwoNumbers(a, b){
+    return a+b;
+}
+
+function subtractTwoNumbers(a, b){
+    return a-b;
+}
+
+
+}
