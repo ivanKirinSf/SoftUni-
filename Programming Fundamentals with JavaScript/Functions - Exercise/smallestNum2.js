c8c4a6062809c4f09a0226a043d006fb8ef0f06f
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-04-07 01:59:37 -0700

    smallestNum2.js
    Write a function that receives three integers and prints the smallest number. Use an appropriate name for the function.
    
    Input: 2,5,3   Output: 2
    Input: 600, 342, 123   Output: 123
    Input: 25, 21, 4   Output: 4
    Input: 2, 2, 2   Output: 2

--- Programming Fundamentals with JavaScript/Functions - Exercise/smallestNum2.js
+++ Programming Fundamentals with JavaScript/Functions - Exercise/smallestNum2.js
@@ -1,1 +1,27 @@
+function smallestOfThreeNumbers(num1, num2, num3){
 
+    let res = smallestNum(num1, num2, num3)
+
+    console.log(res);
+
+
+    function smallestNum(a, b, c){
+        let smallestN = a;
+
+        if(b < smallestN){
+            smallestN = b;
+        }
+        
+        if(c < smallestN){
+            smallestN = c;
+        }
+
+      
+        return smallestN;
+
+        
+    }
+
+    
+
+}
