e361c5238d7cea433115aacd9edfd2c9dbf24e6f
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-04-24 02:05:05 -0700

    nXnMatrix.js
    Write a function that receives a single integer number n and prints nxn matrix with that number.
    
    Input: 3
    Output: 
    3 3 3
    3 3 3
    3 3 3
    
    
    
    Input: 7
    Output: 
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    7 7 7 7 7 7 7
    
    
    
    Input: 2
    Output: 
    2 2
    2 2

---
+++ Programming Fundamentals with JavaScript/Functions - Exercise/nXnMatrix.js
@@ -0,0 +1,31 @@
+function nXnMatrix(n){
+
+let string = line(n);
+
+for(let i = 0; i < n; i++){
+    console.log(string)
+}
+
+    function line(a){
+    let arr = [];
+
+    index = 0;
+    while(index < n){
+
+        let temp = Number(n);
+        arr.push(temp);
+
+        index++
+}
+
+return (arr.join(" "))
+
+    }
+
+
+   
+
+
+}
+
+nXnMatrix(3)
