97e206ce5a616a6cb506b3bce3d06380ffab358d
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-06-18 03:05:03 -0700

    Create sorting2.js
    Write a function that sorts an array of numbers so that the first element is the biggest one, the second is the smallest one, the third is the second biggest one, and the fourth is the second smallest one, and so on. 
    Print the elements on one row, separated by a single space.
    
    Input: [1, 21, 3, 52, 69, 63, 31, 2, 18, 94]
    Output: 94 1 69 2 63 3 52 18 31 21
    
    Input: [34, 2, 32, 45, 690, 6, 32, 7, 19, 47]
    Output: 690 2 47 6 45 7 34 19 32 32

---
+++ Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/sorting2.js
@@ -0,0 +1,34 @@
+function sorting(input){
+
+let sortArr = input.sort((a,b)=> b - a);
+
+let res = [];
+
+for(let i = 0; i < sortArr.length / 2; i++){
+    res.push(sortArr[i]);
+    if(sortArr.length % 2 === 1 && !((sortArr.length - 1) / 2 === i)){
+
+        res.push(sortArr[sortArr.length - 1 - i])
+
+    }else if(sortArr.length % 2 === 0) {
+
+        res.push(sortArr[sortArr.length - 1 - i])
+
+    }
+   
+}
+
+console.log(res.join(" "))
+
+
+}
+
+
+
+
+
+
+
+//sorting([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]);
+console.log("--------------------------------------")
+sorting([34, 2, 32, 45, 690, 6, 32, 7, 19, 47])
