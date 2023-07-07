d0531677f51ef670f4dc80ed76d9f6a20ea82069
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-07-07 08:07:49 -0700

    Update arrayManipulator.js
    Write a function that receives an array of integers and an array of string commands and executes them over the array. The commands are as follows:
    •	add <index> <element> – adds element at the specified index (elements right from this position inclusively are shifted to the right).
    •	addMany <index><element 1> <element 2> … <element n> – adds a set of elements at the specified index.
    •	contains <element> – prints the index of the first occurrence of the specified element (if exists) in the array or -1 if the element is not found.
    •	remove <index> – removes the element at the specified index.
    •	shift <positions> – shifts every element of the array the number of positions to the left (with rotation).
    o	For example, [1, 2, 3, 4, 5] -> shift 2 -> [3, 4, 5, 1, 2]
    •	sumPairs – sums the elements in the array by pairs (first + second, third + fourth, …).
    o	For example, [1, 2, 4, 5, 6, 7, 8] -> [3, 9, 13, 8].
    •	print – stop receiving more commands and print the last state of the array in the following format:
             `[ {element1}, {element2}, …elementN} ]`
      Note: The elements in the array must be joined by comma and space (, ).

--- Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/arrayManipulator.js
+++ Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/arrayManipulator.js
@@ -1,83 +1,80 @@
 function arrayManipulator(arr1, arr2){
 
- for(let i = 0; i<arr2.length; i++){
+for(let i = 0; i < arr2.length; i++){
+
     let temp = arr2[i].split(" ");
 
     if(temp[0] === "add"){
 
         let index = Number(temp[1]);
-
         let element = Number(temp[2]);
 
-        arr1.splice(index, 0, element); 
-
+        arr1.splice(index, 0, element);
+     
     } else if(temp[0] === "addMany"){
 
         let index = Number(temp[1]);
 
-        temp.splice(0,2);
+        temp.splice(0, 2);
 
-        let numToAdd = temp.map(Number)
+        let elements = temp.map(Number);
 
-        arr1.splice(index, 0, ...numToAdd);
-
+        arr1.splice(index, 0, ...elements);     
+        
     } else if(temp[0] === "contains"){
 
         let element = Number(temp[1]);
 
         let index = arr1.indexOf(element);
 
-        console.log(index);
+        console.log(index)
+
 
     } else if(temp[0] === "remove"){
-
+        
         let index = Number(temp[1]);
 
-        arr1.splice(index,1);   
-
-
+        arr1.splice(index, 1);
+        
     } else if(temp[0] === "shift"){
 
         let positions = Number(temp[1]);
-              
-        for(let i = 0; i<positions; i++){
 
-            let temp = arr1.shift();
+        for(let i = 0; i < positions; i++){
 
-            arr1.push(temp)
-        }       
+            let element = arr1.shift();
+
+            arr1.push(element)           
+
+        } 
 
     } else if(temp[0] === "sumPairs"){
 
         let newArr = [];
 
         if(arr1.length % 2 !== 0){
-
-            arr1.push(0);
-
+            arr1.push(0)
         }
 
-        for(let i = 0; i<arr1.length-1; i+=2){
-            let temp1 = arr1[i];
-            let temp2 = arr1[i+1];
+        for(let i = 0; i<arr1.length; i+=2){
+            let temp1 = Number(arr1[i]);
+            let temp2 = Number(arr1[i+1]);
+
             let sum = temp1 + temp2;
 
-            newArr.push(sum);
+            newArr.push(sum)
+
 
         }
 
         arr1 = newArr;        
-               
+
     } else if(temp[0] === "print"){
 
-      console.log(`[ ${arr1.join(", ")} ]`);
-      return               
-
+      return console.log(`[ ${arr1.join(", ")} ]`);     
+      
     }
 
- } 
+}
 
 }
-arrayManipulator([1, 2, 4, 5, 6, 7],
-    ['contains 1', 'print']        
-    )
