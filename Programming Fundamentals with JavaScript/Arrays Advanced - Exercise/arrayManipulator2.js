79f9098ed4b20f67748300f21fedee2e068c1bb4
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-07-19 08:00:37 -0700

    Update arrayManipulator2.js
    Write a function that receives an array of integers and an array of string commands and executes them over the array. The commands are as follows:
    •	add <index> <element> – adds element at the specified index (elements right from this position inclusively are shifted to the right).
    •	addMany <index><element 1> <element 2> … <element n> – adds a set of elements at the specified index.
    •	contains <element> – prints the index of the first occurrence of the specified element (if exists) in the array or -1 if the element is not found.
    •	remove <index> – removes the element at the specified index.
    •	shift <positions> – shifts every element of the array the number of positions to the left (with rotation).
    o	For example, [1, 2, 3, 4, 5] -> shift 2 -> [3, 4, 5, 1, 2]
    •	sumPairs – sums the elements in the array by pairs (first + second, third + fourth, …).
    o	For example, [1, 2, 4, 5, 6, 7, 8] -> [3, 9, 13, 8].
    •	print – stop receiving more commands and print the last state of the array in the following format:
             `[ {element1}, {element2}, …elementN} ]`
      Note: The elements in the array must be joined by comma and space (, ).

--- Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/arrayManipulator2.js
+++ Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/arrayManipulator2.js
@@ -1,93 +1,31 @@
-function arrayManipulator(arr1, arr2){
+function arrayManipulator(numbers, commands){
 
-    
-    let integers = arr1.map(Number);
-    let commands = arr2;
+    for(let command of commands){
+        let temp = command.split(" ");
 
-    for(let i = 0; i<commands.length; i++){
-        let temp = commands[i].split(" ");
-        let command = temp[0];
-
-        switch(command){
-
-            case "add": 
-
+        if(temp[0] === "add"){
             let index = Number(temp[1]);
-
             let element = Number(temp[2]);
 
-            integers.splice(index, 0, element);
-            
-            break;
+            numbers.splice(index, 0, element);            
+        }else if(temp[0] === "addMany"){
 
-            case "addMany":
-
-            //let indexAddMany = Number(temp[1]);
-            //let numsToAdd = temp.splice(2,temp.length-2 ); 
-            //let nums = numsToAdd.map(Number);
-            //integers.splice(indexAddMany, 0, ...nums);
-            let idx = Number(temp[1]);
+            let index = Number(temp[1]);
             temp.splice(0,2);
-            let numToAdd = temp.map(Number);
-            integers.splice(idx, 0, ...numToAdd); 
-            
-            
-            break;
-            
-            case "contains": 
-            let elementContains = Number(temp[1]);
-            let indexContains = integers.indexOf(elementContains);
-            console.log(indexContains)
-            break;
+            let numsToAdd = temp.map(Number);
+            numbers.splice(index, 0, ...numsToAdd);          
 
-            case "remove":
-                let indexRemove = Number(temp[1]);
-                integers.splice(indexRemove, 1);
-                break;
-
-                case "shift": 
-                let positions = Number(temp[1]);
-                let numbers = integers.splice(0,positions); 
-                let num = numbers.length;       
-                for(let i = 0; i < num; i++){
-                    let firstNum = numbers.shift();
-                    integers.push(firstNum);
-                };
-                break; 
-
-                case "sumPairs":
-
-                let arrSum = [];
-                let el = 0;
-                let isOdd = false;
-                
-
-                if(integers.length % 2 !== 0){
-                    isOdd = true;
-                    let element = integers.pop();
-                    el = element
-                }               
-                
-                for(let i = 0; i<integers.length; i+=2){
-                    let temp = Number(integers[i]);
-                    let temp2 = Number(integers[i+1]);
-                    let sum = temp + temp2;
-                    arrSum.push(sum);
-                };
-
-                if(isOdd === true){
-                    arrSum.push(el)
-                }
-
-                integers = arrSum;
-
-                break;
-
-                case "print": 
-
-                console.log(`[ ${integers.join(", ")} ]`);
-                break;    
         }
-    }   
+    }
+
+    console.log(numbers)
+
     
 }
+
+arrayManipulator(
+    [1, 2, 3, 4, 5],
+[
+ //'add 1 8',
+ "addMany 3 9 8 7 6 5",
+  "print"])
