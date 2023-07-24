c5902238237d3bf201a2f0227f08893e0256d8c2
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-07-24 01:41:36 -0700

    Update gladiatorInventory3.js
    As a gladiator, Peter has a cool Inventory. He loves to buy new equipment. You are given Peter’s inventory with all of his equipment -> strings, separated by whitespace. 
    You may receive the following commands:
    •	Buy {equipment}
    •	Trash {equipment}
    •	Repair {equipment}
    •	Upgrade {equipment}-{upgrade}
    If you receive the Buy command, you should add the equipment at the last position in the inventory, but only if it isn't bought already.
    If you receive the Trash command, delete the equipment if it exists.
    If you receive the Repair command, you should repair the equipment if it exists and place it in the last position.
    If you receive the Upgrade command, you should check if the equipment exists and insert after it the upgrade in the following format: "{equipment}:{upgrade}".
    Input / Consrtaints
    You will receive an array of strings. Each element of the array is a command.
    •	In the first input element, you will receive Peter's inventory – a sequence of equipment names, separated by space.
    Output
    As output, you must print Peter's inventory on one line, separated by a space. 
    Constraints
    •	The command will always be valid.
    •	The equipment and Upgrade will be strings and will contain any character, except '-'.
    •	Allowed working time / memory: 100ms / 16MB.
    
    
    Input: ['SWORD Shield Spear',
    'Buy Bag',
    'Trash Shield',
    'Repair Spear',
    'Upgrade SWORD-Steel']
    Output: SWORD SWORD:Steel Bag Spear
    
    Input: ['SWORD Shield Spear',
    'Trash Bow',
    'Repair Shield',
    'Upgrade Helmet-V']
    Output: SWORD Spear Shield

--- Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/gladiatorInventory3.js
+++ Programming Fundamentals with JavaScript/Arrays Advanced - Exercise/gladiatorInventory3.js
@@ -24,21 +24,40 @@ function gladiatorInventory(input){
                 let trashIndex = inventory.indexOf(trashEquipment);
                 if(trashIndex !== -1){
                     inventory.splice(trashIndex, 1)
-                }
+                };
+                break;
+                case "Repair":
+                    let equipmentRepair = temp[1];
+                    let repairIndex = inventory.indexOf(equipmentRepair);
+                    if(repairIndex !== -1){
+                        inventory.splice(repairIndex, 1);
+                        inventory.push(equipmentRepair);
+                    };
+                    break;
+                    case "Upgrade":
+                        let tokensUpgrade = temp[1].split("-");
+                        
+                        let equipmentUpgrade = tokensUpgrade[0];
 
+                        let upgrade = tokensUpgrade[1];                        
+                        
+                        let upgradeIndex = inventory.indexOf(equipmentUpgrade);
+
+                        if(upgradeIndex !== -1){
+                            let text = `${equipmentUpgrade}:${upgrade}`;
+                            inventory.splice(upgradeIndex+1, 0, text);
+                        }
         }
     }
-
-    console.log(inventory)
-    
+    console.log(inventory.join(" "))    
 }
 
 gladiatorInventory (
     [
-    'SWORD Shield Spear',
+    'SWORD Spear Shield',
     //'Buy Bag',
-    'Trash Shield',
+    //'Trash Shield',
     //'Repair Spear',
-    //'Upgrade SWORD-Steel'
+    'Upgrade SWORD-Steel'
 ]
 )
