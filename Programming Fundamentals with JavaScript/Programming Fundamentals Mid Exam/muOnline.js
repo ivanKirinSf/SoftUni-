6a812736cb8b8b079a3f8bd02e30cc0c0f990bcd
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-07-31 23:54:12 -0700

    Update muOnline.js
    You have initial health 100 and initial bitcoins 0. You will be given a string representing the dungeon's rooms. Each room is separated with '|' (vertical bar): "room1|room2|room3…"
    Each room contains a command and a number, separated by space. The command can be:
    •	"potion"
    o	You are healed with the number in the second part. But your health cannot exceed your initial health (100).
    o	First print: "You healed for {amount} hp."
    o	After that, print your current health: "Current health: {health} hp."
    •	"chest"
    o	You've found some bitcoins, the number in the second part.
    o	Print: "You found {amount} bitcoins."
    •	In any other case, you are facing a monster, which you will fight. The second part of the room contains the attack of the monster. You should remove the monster's attack from your health. 
    o	If you are not dead (health <= 0), you've slain the monster, and you should print: "You slayed {monster}."
    o	If you've died, print "You died! Killed by {monster}." and your quest is over. Print the best room you've manage to reach: "Best room: {room}"
    If you managed to go through all the rooms in the dungeon, print on the following three lines: 
    "You've made it!"
    "Bitcoins: {bitcoins}"
    "Health: {health}"
    Input / Constraints
    You receive a string representing the dungeon's rooms, separated with '|' (vertical bar): "room1|room2|room3…".
    
    Input: "rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"
    Output: You slayed rat.
    You slayed bat.
    You healed for 10 hp.
    Current health: 80 hp.
    You slayed rat.
    You found 100 bitcoins.
    You died! Killed by boss.
    Best room: 6

--- Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam/muOnline.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam/muOnline.js
@@ -1,35 +1,58 @@
 function muOnline(string){
 
     let text = string.split("|");
-    let health = 80;
+    let health = 100;
     let bitcoins = 0;
+    let roomCounter = 0;
 
     for(let i = 0; i < text.length; i++){
         let temp = text[i].split(" ");
         let command = temp[0];
+        roomCounter += 1;
         
-
         if(command === "potion"){
             let number = Number(temp[1]);
             if(health < 100){
                 let sum = health + number;
                 if(sum > 100){
                     let amount = 100 - health;
-                    console.log(`You healed for ${amount} hp.`)
+                    console.log(`You healed for ${amount} hp.`);
                     health = 100;
+                    console.log(`Current health: ${health} hp.`);
+                    
                 } else {
                     health = sum;
-                    console.log(`You healed for ${number} hp.`)
+                    console.log(`You healed for ${number} hp.`);
+                    console.log(`Current health: ${health} hp.`);
                 }
+            }            
+        }else if (command === "chest"){
+            let number = Number(temp[1]);
+            bitcoins += number;
+            console.log(`You found ${number} bitcoins.`);
+        }else {
+            let attack = Number(temp[1]);
+            health -= attack;
+            let monster = temp[0];
+            if(health > 0){
+                console.log(`You slayed ${monster}.`)
+            }else{
+             console.log(`You died! Killed by ${monster}.`)
+             return console.log(`Best room: ${roomCounter}`)
             }
             
         }
     }
 
+    console.log(`You've made it!`);
+    console.log(`Bitcoins: ${bitcoins}`);
+    console.log(`Health: ${health}`);
     
-    console.log(health)
+
+    
+    //console.log(health)
 
 }
 
-muOnline("potion 10")
-//muOnline("rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000")
+//muOnline("boss 20")
+muOnline("cat 10|potion 30|orc 10|chest 10|snake 25|chest 110")
