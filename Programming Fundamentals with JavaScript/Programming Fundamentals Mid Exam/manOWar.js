1fc19a8969dfcf7f98d2c5611cc27dd46e7a0544
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-08-02 02:39:27 -0700

    Update manOWar.js
    Create a program that tracks the battle and either chooses a winner or prints a stalemate. On the first line, you will receive the status of the pirate ship, which is a string representing integer sections separated by ">". On the second line, you will receive the same type of status, but for the warship: 
    "{section1}>{section2}>{section3}… {sectionn}"
    On the third line, you will receive the maximum health capacity a section of the ship can reach. 
    The following lines represent commands until "Retire":
    •	"Fire {index} {damage}" - the pirate ship attacks the warship with the given damage at that section. Check if the index is valid and if not, skip the command. If the section breaks (health <= 0) the warship sinks, print the following and stop the program: "You won! The enemy ship has sunken."
    •	"Defend {startIndex} {endIndex} {damage}" - the warship attacks the pirate ship with the given damage at that range (indexes are inclusive). Check if both indexes are valid and if not, skip the command. If the section breaks (health <= 0) the pirate ship sinks, print the following and stop the program:
    "You lost! The pirate ship has sunken."
    •	"Repair {index} {health}" - the crew repairs a section of the pirate ship with the given health. Check if the index is valid and if not, skip the command. The health of the section cannot exceed the maximum health capacity.
    •	"Status" - prints the count of all sections of the pirate ship that need repair soon, which are all sections that are lower than 20% of the maximum health capacity. Print the following:
    "{count} sections need repair."
    In the end, if a stalemate occurs, print the status of both ships, which is the sum of their individual sections, in the following format:
    "Pirate ship status: {pirateShipSum}
    Warship status: {warshipSum}"
    Input
    •	On the 1st line, you are going to receive the status of the pirate ship (integers separated by '>')
    •	On the 2nd line, you are going to receive the status of the warship
    •	On the 3rd line, you will receive the maximum health a section of a ship can reach.
    •	On the following lines, until "Retire", you will be receiving commands.
    Output
    •	Print the output in the format described above.
    Constraints
    •	The section numbers will be integers in the range [1….1000]
    •	The indexes will be integers [-200….200]
    •	The damage will be an integer in the range [1….1000]
    •	The health will be an integer in the range [1….1000]

--- Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam/manOWar.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam/manOWar.js
@@ -3,6 +3,7 @@ function manOWar(input) {
     let piratShip = input[0].split(">");
     let warship = input[1].split(">");
     let maxHealth = Number(input[2]);
+    let sum = 0;
 
     input.splice(0, 3)
 
@@ -15,7 +16,7 @@ function manOWar(input) {
         if (command === "Fire") {
             let index = Number(temp[1]);
             let damage = Number(temp[2]);
-            if (index <= warship.length) {
+            if (index < warship.length) {
                 let sectionHealth = Number(warship[index]);
                 let res = sectionHealth - damage;
                 if (res <= 0) {
@@ -23,7 +24,7 @@ function manOWar(input) {
                 } else {
                     let num = Number(res);
                     warship.splice(index, 1, num);
-
+                    
                 }
 
             } else {
@@ -34,9 +35,9 @@ function manOWar(input) {
             let start = Number(temp[1]);
             let end = Number(temp[2]);
             let damage = Number(temp[3]);
-            if (start < piratShip.length && end < piratShip.length) {
+            if (start < piratShip.length-1 && end < piratShip.length) {
                 for (let i = start; i <= end; i++) {
-                    let sectionHealth = piratShip[i];
+                    let sectionHealth = Number(piratShip[i]);
                     let res = sectionHealth - damage;
                     if (res <= 0) {
                         return console.log(`You lost! The pirate ship has sunken.`);
@@ -52,26 +53,62 @@ function manOWar(input) {
             }
             //console.log(damage)
         }else if (command === "Repair"){
-            let index = 
-        }
+            let index = temp[1];
+            let healthAmount = Number(temp[2]);
+            if(index < piratShip.length){
+                let sectionHealth = Number(piratShip[index]);
+                let res = healthAmount + sectionHealth;
+                if(res > 70){
+                    res = 70;
+                    piratShip.splice(index, 1, res)
+                } else {
+                    piratShip.splice(index, 1, res)
+                }
+                //console.log(res)
+            } else {
+                continue;
+            }
 
-
+            //console.log(healthAmount)
+        }else if (command === "Status"){
+            let healthForRepair = maxHealth*0.2;
+            let sectionsToRepairCounter = 0;
+            //console.log(HealthForRepair)
+            for(let i = 0; i<piratShip.length; i++){
+                let sectionHealth = Number(piratShip[i]);
+                if(sectionHealth < healthForRepair){
+                    sectionsToRepairCounter += 1;
+                }
+            }
+            console.log(`${sectionsToRepairCounter} sections need repair.`)
+        }else if (command === "Retire"){
+            break;
+        }        
         //console.log(command)
     } 
 
-    console.log(piratShip)
+    let sumPiratShip = 0;
+        let sumWarShip = 0;
+        for(let p = 0; p < piratShip.length; p++){
+            let temp = Number(piratShip[p]);
+            sumPiratShip += temp;
+        }
+        for(let w = 0; w < warship.length; w++){
+            let temp = Number(warship[w]);
+            sumWarShip += temp;
+        }        
+        console.log(`Pirate ship status: ${sumPiratShip}` + `\n` + `Warship status: ${sumWarShip}`);     
+    //console.log(piratShip)
 
 }
 
-manOWar((["12>13>11>20>66",
-    "12>22>33>44>55>32>18",
-    "70",
-    //"Fire 2 5",
-    //"Fire 8 100",
-    "Defend 2 3 2",
-    //"Defend 0 3 5",
-    //"Repair 1 33",
-    //"Status",
-    //"Retire"
+manOWar([
+"10>3>4>5>2",
+"6>7>8>9>10>11",
+"20",
+//"Status",
+"Fire -1 10",
+//"Defend 3 4 1",
+//"Repair 0 70",
+//"Retire",
 ])
-)
