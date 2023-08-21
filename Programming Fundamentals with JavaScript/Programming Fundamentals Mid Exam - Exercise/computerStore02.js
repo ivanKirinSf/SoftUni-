e44fc2f15448d6e2e3828b0ffd821207a4e0a113
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-08-21 09:19:52 -0700

    Update computerStore02.js
    Write a program that prints you a receipt for your new computer. You will receive the parts' prices (without tax) until you receive what type of customer this is - special or regular. Once you receive the type of customer you should print the receipt.
    The taxes are 20% of each part's price you receive. 
    If the customer is special, he has a 10% discount on the total price with taxes.
    If a given price is not a positive number, you should print "Invalid price!" on the console and continue with the next price.
    If the total price is equal to zero, you should print "Invalid order!" on the console.
    Input
    •	You will receive numbers representing prices (without tax) until command "special" or "regular":
    Output
    •	The receipt should be in the following format: 
    "Congratulations you've just bought a new computer!
    Price without taxes: {total price without taxes}$
    Taxes: {total amount of taxes}$
    -----------
    Total price: {total price with taxes}$"
    Note: All prices should be displayed to the second digit after the decimal point! The discount is applied only on the total price. Discount is only applicable to the final price!
    
    
    Input: ([
    '1050',
    '200',
    '450',
    '2',
    '18.50',
    '16.86',
    'special'
    ])
    Output: 
    Congratulations you've just bought a new computer!
    Price without taxes: 1737.36$
    Taxes: 347.47$
    -----------
    Total price: 1876.35$

--- Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam - Exercise/computerStore02.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam - Exercise/computerStore02.js
@@ -2,38 +2,46 @@ function computerStore(input){
 
     let sum = 0;
     let command = input.shift();
-    
+
     while(command !== "special"){
         if(command === "regular"){
             break;
         }
+
         let price = Number(command);
-        if( price < 0){
+        if(price < 0){
             console.log("Invalid price!");
             command = input.shift();
             continue;
         }
-
         sum += price;
         command = input.shift();
-
-    }
-    if(sum === 0){
-        return "Invalid order!"
-    }else {
-        let taxes = sum*0.20;
-        let totalPrice = sum*1.20;
-        if (command === "special"){
-            totalPrice *= 0.90;
-        }
-
-        return `Congratulations you've just bought a new computer!
-        Price without taxes: ${sum.toFixed(2)}$
-        Taxes: ${taxes.toFixed(2)}$
-        -----------
-        Total price: ${totalPrice.toFixed(2)}$`        
-
     }
 
+    if(sum <= 0){
+       return console.log("Invalid order!");
+    }
+    let taxes = sum*0.2;
+    let totalPrice = sum*1.2;
+    if(command === "special"){
+        totalPrice = totalPrice*0.9;
+    }
+
+    console.log("Congratulations you've just bought a new computer!\n" +
+    `Price without taxes: ${sum.toFixed(2)}$\n`+
+    `Taxes: ${taxes.toFixed(2)}$\n`+
+    "-----------\n"+
+    `Total price: ${totalPrice.toFixed(2)}$\n`
+    )
 
 }
+
+computerStore([
+    '1050',
+    '200',
+    '450',
+    '2',
+    '18.50',
+    '16.86',
+    'special'
+    ])
