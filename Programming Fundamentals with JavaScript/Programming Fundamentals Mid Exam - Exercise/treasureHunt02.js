a2085013353037ed7cd649ac506eda16ab4e8bb9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-08-26 04:12:19 -0700

    Update treasureHunt02.js

--- Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam - Exercise/treasureHunt02.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Mid Exam - Exercise/treasureHunt02.js
@@ -1,38 +1,58 @@
 function treasureHunt(input){
 
-    let state = input.shift().split("|");
-    let command = input.shift();
-    while(command !== "Yohoho!"){
-        let tokens = command.split(" ");
-        let action = tokens.shift();
-        switch(action){
-            case "Loot":
-                for(let el of tokens){
-                    if(state.includes(el)){
-                        continue;
-                    }
-                    state.unshift(el);
-                }
-                break;
-                case"Drop":
-                let index = Number(tokens[0]);
-                if(index < 0 || index > state.length-1){
-                    continue;
-                }
-                let el = state.splice(index,1);
-                state.push(el); 
-        }
+    let chest = input.shift().split("|");
+    let commands = input;
+    
+    for(let el of commands){
+        let token = commands.shift()
+        while ( token !== "Yohoho!"){
+            let list = token.split(" ");  
+            let command = list.shift();
+            switch(command){
+                case "Loot":
+                    let items = list;
+                    for(let el of items){
+                        if(chest.includes(el)){
+                            continue;
+                        } else {
+                            chest.unshift(el);
+                        }
+                        
+                    };break;
+                    case "Drop":
+                        let index = Number(list);
+                        if(index >= 0 && index < chest.length){
+                        let item = chest[index];
+                        chest.splice(index, 1);
+                        chest.push(item);
+                        }
+                        
 
-        command = input.shift();
+                        //console.log(chest)
+                        
+
+
+                    //console.log(items)
+            }       
+            
+            //console.log(command);
+
+            token = commands.shift()
+
+            
+            
+        }
     }
 
-    console.log(state.join(" "))
+    console.log(chest)
+    
+
 
 }
 
 treasureHunt(["Gold|Silver|Bronze|Medallion|Cup",
-"Loot Wood Gold Coins",
-"Loot Silver Pistol",
+//"Loot Wood Gold Coins",
+//"Loot Silver Pistol",
 "Drop 3",
-"Steal 3",
+//"Steal 3",
 "Yohoho!"])
