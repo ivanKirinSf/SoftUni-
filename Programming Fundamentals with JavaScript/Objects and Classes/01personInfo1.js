9323751a3f4a571e6673765193c3082d652e4ea1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-08-31 09:34:33 -0700

    Create 01personInfo1.js
    Write a function that receives 3 parameters, sets them to an object, and returns that object.
    The input comes as 3 separate strings in the following order: firstName, lastName, age.
    
    Input: 
    "Peter", 
    "Pan",
    "20"
    
    Output: 
    firstName: Peter
    lastName: Pan
    age: 20

---
+++ Programming Fundamentals with JavaScript/Objects and Classes/01personInfo1.js
@@ -0,0 +1,17 @@
+function personInfo(firstName, lastName, age){
+
+    let myPersonInfo = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+
+    return myPersonInfo
+
+}
+
+console.log(personInfo(
+    "Ivan",
+    "Kirin",
+    "34",
+))
