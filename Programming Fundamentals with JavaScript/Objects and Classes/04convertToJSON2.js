734fe04156aa1afb8825113a7d0ef887825df1ef
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-09-18 01:26:16 -0700

    Create 04convertToJSON2.js
    Write a function that receives a first name, last name, hair color and sets them to an object.
    Convert the object to JSON string and print it.
    Input is provided as 3 single strings in the order stated above.
    
    
    Input: 'George', 'Jones', 'Brown'
    
    Output: {"name":"George","lastName":"Jones","hairColor":"Brown"}

---
+++ Programming Fundamentals with JavaScript/Objects and Classes/04convertToJSON2.js
@@ -0,0 +1,13 @@
+function convertToJSON(name, lastName, hairColor){
+
+    let obj = {};
+
+    obj.name = name,
+    obj.lastName = lastName,
+    obj.hairColor = hairColor
+
+    console.log(JSON.stringify(obj))
+
+}
+
+convertToJSON('George', 'Jones', 'Brown')
