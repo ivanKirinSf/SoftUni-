4ca5b3b67fa2ab40093585935543f1f8d69808e1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-09-27 06:59:39 -0700

    Update 03storeProvision1.js
    You will receive two arrays. The first array represents the current stock of the local store. The second array will contain products that the store has ordered for delivery.
    The following information applies to both arrays:
    Every even index will hold the name of the product and every odd index will hold the quantity of that product. The second array could contain products that are already in the local store. If that happens increase the quantity for the given product. You should store them into an object, and print them in the following format: (product -> quantity)
    All of the arrays’ values will be strings.
    
    Input: 
    [
    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
    ],
    [
    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
    ]
    
    Output: 
    Chips -> 5
    CocaCola -> 9
    Bananas -> 44
    Pasta -> 11
    Beer -> 2
    Flour -> 44
    Oil -> 12
    Tomatoes -> 70

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/03storeProvision1.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/03storeProvision1.js
@@ -1,79 +1,38 @@
-function storeProvision(arr1, arr2){
+function storeProvision(stocks, orders){
 
-    
-    let stock = 0;
-    let stockNum = 0;
-    let order = 0;
-    let orderNum = 0;
-    
-    let obj = [];
+    let shop = {};
 
-
-    
-
-    for(let i = 0; i<arr1.length; i+=2){
-
-        stock = arr1[i];
-
-        let index = arr2.indexOf(stock);               
-
-        if(index !== -1){
-
-            stockNum = Number(arr1[i+1]);  
-            
-            orderNum = Number(arr2[index+1]);
-
-            let sum = stockNum + orderNum;
-
-            arr1.splice(i+1,1,sum);
-
-            arr2.splice(index, 2)
-
-        }else {
-
-        order = arr2.shift();
-        orderNum = Number(arr2.shift()); 
-            arr1.push(order);
-            arr1.push(orderNum);
-        }
-
-        if(arr2.length === 0){
-            break;
-        }        
-     
-        }
-
-        for(let k = 0; k < arr1.length; k+=2){
-
-           let product = arr1[k];
-            let productNum = Number(arr1[k+1]);
-
-            obj.push(new Object(product, productNum))
-
-            for(let [value,key] of obj){
-                console.log(`${key} -> ${value}`)
-            }
-
-            //products.product = product;
-           // products.productNum = productNum; 
-            
-            //for(let key of Object.keys(products)){
-                //console.log(`${key} -> ${products[key]}`)
-            }
-        }
-
-       
-
-
+    for(let i = 0; i<stocks.length; i+=2){
+        let name = stocks[i];
         
+        let num = stocks[i+1];
 
-        
+        shop[name] = Number(num);
+    }
 
+    for(let i = 0; i<orders.length; i+=2){
+        let name = orders[i];
+        let num = orders[i+1];
+
+        if(shop.hasOwnProperty(name)){
+            shop[name] += Number(num);
+        }else{
+            shop[name] = Number(num);
+        }
+    
+    }
+
+    for(let product of Object.keys(shop)){
+        console.log(`${product} -> ${shop[product]}`)
+    }
+
+    //console.table(shop)
+
+}
 
 storeProvision([
     'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
     ],
     [
     'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
-    ]       
-    )
+    ])
