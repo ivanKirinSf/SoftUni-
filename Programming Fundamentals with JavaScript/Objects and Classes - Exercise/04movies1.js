9516cad550446c06de9d999a13e9bc965abd4281
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-10-02 10:29:47 -0700

    Update 04movies1.js
    Write a function that stores information about movies inside an array. The movie's object info must be name, director, and date. You can receive several types of input:
    •	"addMovie {movie name}" – add the movie
    •	"{movie name} directedBy {director}" – check if the movie exists and then add the director
    •	"{movie name} onDate {date}" – check if the movie exists and then add the date
    At the end print all the movies that have all the info (if the movie has no director, name, or date, don’t print it) in JSON format.
    
    Input: [
    'addMovie Fast and Furious',
    'addMovie Godfather',
    'Inception directedBy Christopher Nolan',
    'Godfather directedBy Francis Ford Coppola',
    'Godfather onDate 29.07.2018',
    'Fast and Furious onDate 30.07.2018',
    'Batman onDate 01.08.2018',
    'Fast and Furious directedBy Rob Cohen'
    ]
    
    Output: 
    {"name":"Fast and Furious","date":"30.07.2018","director":"Rob Cohen"}
    {"name":"Godfather","director":"Francis Ford Coppola","date":"29.07.2018"}

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
@@ -2,32 +2,62 @@ function movies(input){
 
     
     let films = {};
+    let arr = [];
     
         for(let i = 0; i<input.length; i++){
         let movieInfo = input[i].split(" ");
+        //console.log(movieInfo)
 
         if(movieInfo.indexOf("addMovie") !== -1){
-            let index = movieInfo.indexOf("addMovie");
             movieInfo.shift();
-            let filmName = movieInfo.join(" ");
-            films.name = filmName            
-        }//else if(movieInfo.indexOf("directedBy") !== -1){
-            //let index = movieInfo.indexOf("directedBy");
-           // let director = movieInfo.slice(index+1, movieInfo.length).join(" ");
-            //let film = movieInfo.slice(0, index).join(" ");
+            let film = movieInfo.join(" ");
+            arr.push(film);
+            arr.push("date");
+            arr.push("director")
+            //console.log(film.join(" "))
+        }else if(movieInfo.indexOf("directedBy") !== -1){
+            let index = movieInfo.indexOf("directedBy");
+            let filmName = movieInfo.slice(0, index).join(" ");
+            let director = movieInfo.slice(index+1, movieInfo.length).join(" ")
+            //console.log(filmName);
+            if(arr.indexOf(filmName) !== -1){
+                let index = arr.indexOf(filmName);
+                arr.splice(index + 2, 1, director)
+            }    
+        }else if(movieInfo.indexOf("onDate") !== -1){
+            let index = movieInfo.indexOf("onDate");
+            let filmName = movieInfo.slice(0, index).join(" ");
+            let date = movieInfo.slice(index+1,movieInfo.length).join(" ");
+            //console.log(director);
+            if(arr.indexOf(filmName) !== -1){
+                let index = arr.indexOf(filmName);
+                arr.splice(index + 1, 1, date);
+            }
 
-            //if(arrFilms.indexOf(film) !== -1){
-            //    let indexArrFilms = arrFilms.indexOf(film);
-            //    arrFilms.splice(indexArrFilms, 0, )
-            //}
+        }
+    }    
 
-            //console.log(film)
-        //}        
+    //console.log(arr)
+    
+    for(let i = 0; i < arr.length; i++){
+        let film = arr.shift();
+        let date = arr.shift();
+        let director = arr.shift();
+
+        films.name = film;
+        films.date = date;
+        films.director = director;
+
+        console.log(films)
+       // for(let key of Object.keys(films)){
+           // console.log(`${key}:${films[key]}`)
+       // }
     }
+    
+        //console.log(films)
+    }
+   
 
-    console.log(films)
-
-}
 
 
 
@@ -40,5 +70,5 @@ movies([
     'Fast and Furious onDate 30.07.2018',
     'Batman onDate 01.08.2018',
     'Fast and Furious directedBy Rob Cohen'
-    ]
+    ]    
     )
