61fd61fc279d13cad1d5952e791b1413f7400ef7
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-10-03 13:31:13 -0700

    Update 04movies1.js

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
@@ -1,8 +1,8 @@
 function movies(input){
 
-    
-    let films = {};
     let arr = [];
+    let films = {};
+    
     
         for(let i = 0; i<input.length; i++){
         let movieInfo = input[i].split(" ");
@@ -11,64 +11,83 @@ function movies(input){
         if(movieInfo.indexOf("addMovie") !== -1){
             movieInfo.shift();
             let film = movieInfo.join(" ");
-            arr.push(film);
-            arr.push("date");
-            arr.push("director")
+            films.name = film;
+            arr.push(films);
+            films = {};
             //console.log(film.join(" "))
         }else if(movieInfo.indexOf("directedBy") !== -1){
             let index = movieInfo.indexOf("directedBy");
             let filmName = movieInfo.slice(0, index).join(" ");
             let director = movieInfo.slice(index+1, movieInfo.length).join(" ")
             //console.log(filmName);
-            if(arr.indexOf(filmName) !== -1){
-                let index = arr.indexOf(filmName);
-                arr.splice(index + 2, 1, director)
+            let isExist = arr.findIndex(e=>e.name===filmName);
+            if(isExist !== -1){                
+                arr[isExist].director = director;
             }    
         }else if(movieInfo.indexOf("onDate") !== -1){
             let index = movieInfo.indexOf("onDate");
             let filmName = movieInfo.slice(0, index).join(" ");
             let date = movieInfo.slice(index+1,movieInfo.length).join(" ");
             //console.log(director);
-            if(arr.indexOf(filmName) !== -1){
-                let index = arr.indexOf(filmName);
-                arr.splice(index + 1, 1, date);
+            let isExist = arr.findIndex(e=>e.name === filmName)
+            if(isExist !== -1){
+                arr[isExist].date = date;
             }
-
         }
-    }    
+
+        //console.log(JSON.stringify(films))
+    }   
+    
+    
+
+    for(let k = 0; k<arr.length; k++){
+        let temp = arr[k];
+        let dateCheck = temp.date;
+        let directorCheck = temp.director;
+        
+
+        if(dateCheck.length !== 0 && directorCheck.length !== 0){
+            console.log(JSON.stringify(temp))
+        }
+        
+    }
 
     //console.log(arr)
     
-    for(let i = 0; i < arr.length; i++){
-        let film = arr.shift();
-        let date = arr.shift();
-        let director = arr.shift();
+    //console.log(JSON.stringify(films))
 
-        films.name = film;
-        films.date = date;
-        films.director = director;
+    //console.log(arr)
+    
+    //for(let i = 0; i < arr.length; i++){
+        //let film = arr.shift();
+        //let date = arr.shift();
+        ///let director = arr.shift();
 
-        console.log(films)
+        //films.name = film;
+        //films.date = date;
+        //films.director = director;
+
+        //console.log(JSON.stringify(films))
        // for(let key of Object.keys(films)){
            // console.log(`${key}:${films[key]}`)
        // }
     }
+
+    
     
         //console.log(films)
-    }
-   
+    
+    
 
 
 
 
 movies([
-    'addMovie Fast and Furious',
-    'addMovie Godfather',
-    'Inception directedBy Christopher Nolan',
-    'Godfather directedBy Francis Ford Coppola',
-    'Godfather onDate 29.07.2018',
-    'Fast and Furious onDate 30.07.2018',
-    'Batman onDate 01.08.2018',
-    'Fast and Furious directedBy Rob Cohen'
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'
     ]    
     )
