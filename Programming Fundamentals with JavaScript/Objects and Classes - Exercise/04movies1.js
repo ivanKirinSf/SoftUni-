a7f355586128fa3dbef584eae6dae53b8b11f9e1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-10-04 13:17:25 -0700

    Update 04movies1.js
    Write a function that stores information about movies inside an array. The movie's object info must be name, director, and date. You can receive several types of input:
    •	"addMovie {movie name}" – add the movie
    •	"{movie name} directedBy {director}" – check if the movie exists and then add the director
    •	"{movie name} onDate {date}" – check if the movie exists and then add the date
    At the end print all the movies that have all the info (if the movie has no director, name, or date, don’t print it) in JSON format.
    
    
    Input: [
    'addMovie Fast and Furious',
    'addMovie Godfather',
    'Inception directedBy Christopher Nolan',
    'Godfather directedBy Francis Ford Coppola',
    'Godfather onDate 29.07.2018',
    'Fast and Furious onDate 30.07.2018',
    'Batman onDate 01.08.2018',
    'Fast and Furious directedBy Rob Cohen'
    ]
    
    Output: 
    {"name":"Fast and Furious","date":"30.07.2018","director":"Rob Cohen"}
    {"name":"Godfather","director":"Francis Ford Coppola","date":"29.07.2018"}

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/04movies1.js
@@ -1,88 +1,55 @@
 function movies(input){
 
     let arr = [];
-    let films = {};
-    
-    
-        for(let i = 0; i<input.length; i++){
-        let movieInfo = input[i].split(" ");
-        //console.log(movieInfo)
+    let films = {}
 
+    for(let i = 0; i<input.length; i++){
+        let movieInfo = input[i].split(" ");
         if(movieInfo.indexOf("addMovie") !== -1){
             movieInfo.shift();
-            let film = movieInfo.join(" ");
-            films.name = film;
+            let filmName = movieInfo.join(" ");
+            films.name = filmName;
             arr.push(films);
             films = {};
-            //console.log(film.join(" "))
         }else if(movieInfo.indexOf("directedBy") !== -1){
             let index = movieInfo.indexOf("directedBy");
-            let filmName = movieInfo.slice(0, index).join(" ");
-            let director = movieInfo.slice(index+1, movieInfo.length).join(" ")
-            //console.log(filmName);
-            let isExist = arr.findIndex(e=>e.name===filmName);
-            if(isExist !== -1){                
-                arr[isExist].director = director;
-            }    
+            let filmName = movieInfo.slice(0,index).join(" ");
+            let directorName = movieInfo.slice(index+1,movieInfo.length).join(" ");
+            //console.log(director)
+            let isExist = arr.findIndex(k=>k.name === filmName)
+            if(isExist !== -1){
+                arr[isExist].director = directorName;                
+            }
+            //console.log(filmName)
         }else if(movieInfo.indexOf("onDate") !== -1){
             let index = movieInfo.indexOf("onDate");
-            let filmName = movieInfo.slice(0, index).join(" ");
-            let date = movieInfo.slice(index+1,movieInfo.length).join(" ");
-            //console.log(director);
-            let isExist = arr.findIndex(e=>e.name === filmName)
+            let filmName = movieInfo.slice(0,index).join(" ");
+            let filmDate = movieInfo.slice(index+1, movieInfo.length).join(" ");
+            let isExist = arr.findIndex(j=>j.name === filmName);
             if(isExist !== -1){
-                arr[isExist].date = date;
+                arr[isExist].date = filmDate;
             }
-        }
 
-        //console.log(JSON.stringify(films))
-    }   
-    
-    
+            //console.log(isExist)
+        }
+        
+    }
 
     for(let k = 0; k<arr.length; k++){
         let temp = arr[k];
-        let dateCheck = temp.date;
         let directorCheck = temp.director;
-        
+        let dateCheck = temp.date;
 
-        if(dateCheck.length !== 0 && directorCheck.length !== 0){
+        if(directorCheck && dateCheck){
             console.log(JSON.stringify(temp))
         }
-        
     }
 
     //console.log(arr)
-    
-    //console.log(JSON.stringify(films))
+}
 
-    //console.log(arr)
-    
-    //for(let i = 0; i < arr.length; i++){
-        //let film = arr.shift();
-        //let date = arr.shift();
-        ///let director = arr.shift();
-
-        //films.name = film;
-        //films.date = date;
-        //films.director = director;
-
-        //console.log(JSON.stringify(films))
-       // for(let key of Object.keys(films)){
-           // console.log(`${key}:${films[key]}`)
-       // }
-    }
-
-    
-    
-        //console.log(films)
-    
-    
-
-
-
-
-movies([
+movies(
+    [
     'addMovie The Avengers',
     'addMovie Superman',
     'The Avengers directedBy Anthony Russo',
