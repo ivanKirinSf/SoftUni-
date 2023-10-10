bbe3404c9efec7e0d6cd593a7fe1ae5ec7679c36
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-10-10 11:33:26 -0700

    Update 05inventory1.js
    Create a function, which creates a register for heroes, with their names, level, and items (if they have such). 
    The input comes as an array of strings. Each element holds data for a hero, in the following format:
    "{heroName} / {heroLevel} / {item1}, {item2}, {item3}..." 
    You must store the data about every hero. The name is a string, a level is a number and the items are all strings.
    The output is all of the data for all the heroes you’ve stored sorted ascending by level. The data must be in the following format for each hero:
    Hero: {heroName}
    level => {heroLevel}
    Items => {item1}, {item2}, {item3}
    
    Input:
     [
    'Isacc / 25 / Apple, GravityGun',
    'Derek / 12 / BarrelVest, DestructionSword',
    'Hes / 1 / Desolator, Sentinel, Antara'
    ]
    Output:
    Hero: Hes
    level => 1
    items => Desolator, Sentinel, Antara
    Hero: Derek
    level => 12
    items => BarrelVest, DestructionSword
    Hero: Isacc
    level => 25
    items => Apple, GravityGun

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/05inventory1.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/05inventory1.js
@@ -1,31 +1,36 @@
 function inventory(input){
 
-let res = [];
+    let res = [];
 
-for(let line of input){
-    let [name, level, items] = line.split(" / ");
-    let heroes = {
-        heroes: name,
-        heroes: Number(level),
-        heroes: items,
+    for (let line of input){
+        let [name, level, items] = line.split(" / ");
+        let heroes = {
+            name: name,
+            level: Number(level),
+            item: items
+        }
+        res.push(heroes)
+        //console.log(heroes)
+
+        
     }
-    res.push(heroes)
-}
 
-res.sort((heroesA, heroesB) => heroesA - heroesB);
+    res.sort((heroesA, heroesB) => heroesA.level - heroesB.level);
+    //console.log(res)
 
-for(let heroes of res){
-    console.log(`Hero: ${heroes[`name`]}`);
-    console.log(`level : ${heroes.level}`);
-    console.log(`items: ${heroes.items}`);
-}
-
+    for(let heroes of res){
+        console.log(`Hero: ${heroes.name}`);
+        console.log(`level => ${heroes.level}`);
+        console.log(`items => ${heroes.item}`);
+    }
 
 }
 
 inventory([
-    'Batman / 18 / Banana, Gun',
-    'Superman / 0 / Sword',
-    'Poppy / 28 / Sentinel, Antara'
-    ]    
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+    
+    
     )
