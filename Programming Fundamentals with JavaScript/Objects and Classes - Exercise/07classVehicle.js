6920f08e4de04c846e43f52c9c706cc6d938f324
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-10-21 18:21:21 +0300

    Update 07classVehicle.js

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/07classVehicle.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/07classVehicle.js
@@ -1,22 +1,21 @@
-function classVehicle(input){
-
-    class vehicle {
-        constructor(type, model, parts, fuel, drive){
-            this.type = type;
-            this.model = model;
-            this.parts = parts;
-            this.fuel = fuel;
-            this.drive = drive;
-        }
+class Vehicle{
+    constructor(type, model, parts, fuel){
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.fuel = fuel;
+        this.parts.quality = parts.engine*parts.power;
     }
 
-    let car = new vehicle()
-
+    drive(fuelLoss){
+        this.fuel -= fuelLoss;
+    }
 }
 
+
+
 let parts = { engine: 6, power: 100 };
-    let vehicle = new Vehicle('a', 'b', parts, 200);
-    vehicle.drive(100);
-    console.log(vehicle.fuel);
-    console.log(vehicle.parts.quality);
-    
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
