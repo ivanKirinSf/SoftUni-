45e230bd285415d238f6faf85f1c7e4f18cc60c9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-06 19:33:32 +0200

    Update 01phoneBook.js

--- Programming Fundamentals with JavaScript/Associative Arrays/01phoneBook.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/01phoneBook.js
@@ -1,35 +1,23 @@
 function phoneBook(input){
 
-    let arr = input;
+let dict = {};
 
-    let name = [];
-
-    for(let i = 0; i<arr.length; i++){
-
-        let temp = arr[i].split(" ");
-        let obj = {};
-        let key = temp.shift();
-        let value = temp.shift();
-        //console.log(value)
-        obj.name = key;
-        obj.num = value;
-
-        if(name.indexOf(obj[name]) === -1){
-        name.push(obj)
-        //arr[key] = temp;
-        }        
-
-        
-    }
-
-    console.log(name)
-
+for(let el of input){
+    let temp = el.split(" ");
+    let name = temp[0];
+    let phone = temp[1];
+    dict[name] = phone
     
+}
+
+for(let key in dict){
+    console.log(`${key} -> ${dict[key]}`)
+}
 
 }
 
 phoneBook(['Tim 0834212554',
- 'Tim 0877547887',
- //'Bill 0896543112',
- //'Tim 0876566344'
-])
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+)
