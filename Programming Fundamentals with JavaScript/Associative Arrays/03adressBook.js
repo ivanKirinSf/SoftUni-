ea70aff74579bc11115d3fd7ac17b889abdd3e92
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-13 22:55:04 +0200

    Update 03adressBook.js

--- Programming Fundamentals with JavaScript/Associative Arrays/03adressBook.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/03adressBook.js
@@ -1,22 +1,34 @@
-function adressBook(input){
+function addressBook(input){
 
-    let dict = {};
+    let obj = {};
 
-    for(let str of input){
-        let temp = str.split(":");
-        let name = temp[0];
-        let number = temp[1];
-        dict[name] = number;
+    for(let el of input){
+        let temp = el.split(":");
+        let name = temp.shift();
+        let adress = temp.shift();
+        obj[name] = adress;        
     }
 
-    let entries = Object.entries(dict).sort(([aKey], [bKey]) => [aKey].localeCompare([bKey]));
+    //console.log(obj);
+    let entries = Object.entries(obj).sort((a,b)=> a[0].localeCompare(b[0]));
+    for(let  str of entries){
+        let temp = str;
+        let key = temp[0];
+        let value = temp[1];
 
-    console.log(entries)
-    
+        console.log(`${key} -> ${value}`)
+    }
 }
 
-adressBook(
-    ['Tim:Doe Crossing',
- 'Bill:Nelson Place',
- 'Peter:Carlyle Ave',
- 'Bill:Ornery Rd'])
+addressBook(['Bob:Huxley Rd',
+'John:Milwaukee Crossing',
+'Peter:Fordem Ave',
+'Bob:Redwing Ave',
+'George:Mesta Crossing',
+'Ted:Gateway Way',
+'Bill:Gateway Way',
+'John:Grover Rd',
+'Peter:Huxley Rd',
+'Jeff:Gateway Way',
+'Jeff:Huxley Rd']
+)
