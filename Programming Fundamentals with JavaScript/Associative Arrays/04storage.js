cadd70661031c2c8425d9476160b45ca0b5b119a
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-14 19:50:40 +0200

    Update 04storage.js

--- Programming Fundamentals with JavaScript/Associative Arrays/04storage.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/04storage.js
@@ -1,6 +1,6 @@
 function storage(input){
 
-    let map = new map();
+    let map = new Map();
 
     for(let el of input){
         let tokens = el.split(" ");
@@ -9,22 +9,25 @@ function storage(input){
 
         if(map.has(product)){
             let cuurrentQuantity = map.get(product);
-            map.set(product, cuurrentQuantity + quantity);
+            map.set(product, quantity + cuurrentQuantity)
         }else{
             map.set(product, quantity)
         }
     }
+    
+    for(let kv of map){
 
-    for(let kv of input){
         let [key, value] = kv;
 
         console.log(`${key} -> ${value}`)
+        
     }
 
-
 }
+
 storage(['tomatoes 10',
 'coffee 5',
 'olives 100',
 'coffee 40']
+
 )
