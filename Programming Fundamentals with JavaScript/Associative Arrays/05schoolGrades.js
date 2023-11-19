57ab39651f76a772c6ac180c2895883d5bf0b1dc
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-19 19:54:39 +0200

    Update 05schoolGrades.js

--- Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
@@ -1,24 +1,51 @@
 function schoolGrades(input){
 
    let socArr = new Map();
+   let gradesArr = [];
    
    for(let el of input){
      let temp = el.split(" ");
      let name = temp.shift();
      let grades = temp;
      let totalGrade = 0;
-     let averageGrade = 0;
-
-     for(let el of grades){
-        totalGrade += Number(el);
-    }
-
-    averageGrade = totalGrade/grades.length;
+     let averageGrade = 0; 
+     let totalAverage = 0;   
 
     if(socArr.has(name)){
+
+
+        if(gradesArr.length === 0){
+
         let oldGrade = socArr.get(name);
-        socArr.set(name, averageGrade + oldGrade);
+        gradesArr.push(oldGrade);
+
+        }
+        
+         
+        grades.map(Number);
+        
+        for(let el of grades){
+            totalGrade += Number(el);
+        }
+
+        averageGrade = totalGrade/grades.length;
+        
+        gradesArr.push(averageGrade);
+
+        for(let el of gradesArr){
+            totalAverage += el;
+
+            socArr.set(name, totalAverage);
+        }
+            
+        //socArr.set(name, (averageGrade + oldGrade)/2);
+
     }else{
+        for(let el of grades.map(Number)){
+            totalGrade += Number(el);
+        }
+    
+        averageGrade = totalGrade/grades.length;
         socArr.set(name, averageGrade)        
     }
     
@@ -32,8 +59,11 @@ function schoolGrades(input){
 
 }
 
-schoolGrades(['Steven 3 5 6 4',
-'George 4 6',
-'Tammy 2 5 3',
-'Steven 6 3']
+schoolGrades([
+//'Lilly 4 6 6 5',
+'Tim 5 6',
+//'Tammy 2 4 3',
+'Tim 6 6',
+'Tim 3 4 5'
+]
 )
