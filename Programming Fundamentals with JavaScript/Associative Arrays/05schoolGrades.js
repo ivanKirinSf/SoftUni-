0864e6d109abdff7bb80704563649f9f6d1f5d6a
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-22 23:16:58 +0200

    Update 05schoolGrades.js

--- Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
@@ -1,68 +1,50 @@
 function schoolGrades(input){
 
-   let socArr = new Map();
-   let gradesArr = [];
-   
-   for(let el of input){
-     let temp = el.split(" ");
-     let name = temp.shift();
-     let grades = temp;
-     let totalGrade = 0;
-     let averageGrade = 0; 
-     let totalAverage = 0;   
+    let studentsGrades = {};
+    //let arrGrades = [];
 
-    if(socArr.has(name)){
+    for(let el of input){
+        let temp = el.split(" ");
+        let name = temp.shift();
+        let grades = temp.map(Number);
+        if(studentsGrades.hasOwnProperty(name)){
 
+            for(let i of grades){
+                studentsGrades[name].push(i)
+            }
 
-        if(gradesArr.length === 0){
+        }else {
 
-        let oldGrade = socArr.get(name);
-        gradesArr.push(oldGrade);
+            studentsGrades[name] = grades;
 
         }
-        
-         
-        grades.map(Number);
-        
-        for(let el of grades){
-            totalGrade += Number(el);
-        }
-
-        averageGrade = totalGrade/grades.length;
-        
-        gradesArr.push(averageGrade);
-
-        for(let el of gradesArr){
-            totalAverage += el;            
-        }
-
-        socArr.set(name, totalAverage/gradesArr.length);
-            
-        //socArr.set(name, (averageGrade + oldGrade)/2);
-
-    }else{
-        for(let el of grades.map(Number)){
-            totalGrade += Number(el);
-        }
-
-        averageGrade = totalGrade/grades.length;
-        socArr.set(name, averageGrade)        
+        //console.log(grades)
     }
-    
-   }
 
-   socArr = new Map([...socArr.entries()].sort());
+    function average(input){
+        let sum = 0;
+        let average = 0;
+        for(let el of input){
+            let temp = Number(el);
+            sum += temp;
+        }
 
-   for(let [name, grades] of socArr){
-    console.log(`${name}: ${grades.toFixed(2)}`)
-   }
+        average = sum / input.length;
+        console.log(average)
+    }
 
+    for(let key of studentsGrades){
+        average(studentsGrades[key])
+    }
+
+    //console.log(studentsGrades)
 }
 
-schoolGrades(
-    ['Steven 3 5 6 4',
-'George 4 6',
-'Tammy 2 5 3',
-'Steven 6 3']
+schoolGrades([
+'Lilly 4 6 6 5',
+'Tim 5 6',
+'Tammy 2 4 3',
+'Tim 6 6'
+])
 
-)
+
