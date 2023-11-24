3fb7a4caf6a6c7f678b6ec9807bb0379b38be774
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-25 00:20:36 +0200

    Update 05schoolGrades.js

--- Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
@@ -1,50 +1,47 @@
 function schoolGrades(input){
 
-    let studentsGrades = {};
-    //let arrGrades = [];
+    let studentInfo = [];
 
     for(let el of input){
         let temp = el.split(" ");
         let name = temp.shift();
         let grades = temp.map(Number);
-        if(studentsGrades.hasOwnProperty(name)){
+        //console.log(grades);
 
+        if(studentInfo.hasOwnProperty(name)){
             for(let i of grades){
-                studentsGrades[name].push(i)
+                studentInfo[name].push(i)
             }
-
         }else {
-
-            studentsGrades[name] = grades;
-
+            studentInfo[name] = grades;
         }
-        //console.log(grades)
     }
 
-    function average(input){
-        let sum = 0;
-        let average = 0;
-        for(let el of input){
-            let temp = Number(el);
-            sum += temp;
+        function average(input){
+            for(let el of input){
+                let total = 0;
+                total += el;
+
+                let average = total/input.length;
+                studentInfo[key] = average.toFixed(2);
+            }
         }
 
-        average = sum / input.length;
-        console.log(average)
+        for(let key of studentInfo){
+
+            average(studentInfo[key]);
+        }      
+
+        let entries = Object.entries(studentInfo);
+
+            entries.sort((a, b) => a[0].localeCompare(b[0]));
+
+            for(let key of entries){
+                console.log(`${key}: ${entries[key]} `)
+            }     
+
+
     }
 
-    for(let key of studentsGrades){
-        average(studentsGrades[key])
-    }
-
-    //console.log(studentsGrades)
-}
-
-schoolGrades([
-'Lilly 4 6 6 5',
-'Tim 5 6',
-'Tammy 2 4 3',
-'Tim 6 6'
-])
 
 
