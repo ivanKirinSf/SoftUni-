0c2c97d787fb845ce61608b8cb30994750243ca2
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-27 20:13:20 +0200

    Update 05schoolGrades.js

--- Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
+++ Programming Fundamentals with JavaScript/Associative Arrays/05schoolGrades.js
@@ -1,20 +1,19 @@
 function schoolGrades(input){
 
-let studentsInfo =[];
+let studentsInfo = [];
 
 for(let el of input){
     let temp = el.split(" ");
+
     let name = temp.shift();
     let grades = temp.map(Number);
 
-    console.log(grades);
-
     if(studentsInfo.hasOwnProperty(name)){
 
-        for(let i of grades){
-            studentsInfo[name].push(i);
+        for(let el of grades){
+            studentsInfo[name].push(el)
         }
-        
+
     }else{
         studentsInfo[name] = grades;
     }
@@ -23,38 +22,35 @@ for(let el of input){
 function average(input){
     let totalGrades = 0;
     let averageGrade = 0;
-
-    for(let el of input){
-        totalGrades += el;
-    }
-    averageGrade = totalGrades/input.length;
-        
-        //console.log(averageGrade)
-        studentsInfo[el] = averageGrade.toFixed(2);
-}
-
-for (el in studentsInfo){
-    average(studentsInfo[el])
-}
-
-studentsInfo.sort((a,b) => a[0].localeCompare(b[0]));
-
-let entries = Object.entries(studentsInfo);
-for(key in entries){
-
-    console.log(`${key}: ${entries[key]}`)
-
-}
-
+    for(let i of input){
+        totalGrades += i;
+        averageGrade = totalGrades/input.length;
 
     }
 
+    studentsInfo[key] = averageGrade.toFixed(2);
+
+}
+
+for(key in studentsInfo){
+    average(studentsInfo[key])
+}
+
+let sorted = Object.keys(studentsInfo).sort((a, b) => a.localeCompare(b));
+
+//console.table(sorted)
+
+for(let el of sorted){
+    console.log(`${el}: ${studentsInfo[el]}`)
+}
+
+    }
 
 schoolGrades([
-//'Lilly 4 6 6 5',
+'Lilly 4 6 6 5',
 'Tim 5 6',
-//'Tammy 2 4 3',
-//'Tim 6 6'
+'Tammy 2 4 3',
+'Tim 6 6'
 ])
 
 
