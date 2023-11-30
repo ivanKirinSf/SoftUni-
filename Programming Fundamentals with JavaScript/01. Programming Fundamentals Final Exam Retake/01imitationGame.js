02939c0069b3fa95f96ad07373cb8852cd801565
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-11-30 23:48:54 +0200

    Update 01imitationGame.js

--- Programming Fundamentals with JavaScript/01. Programming Fundamentals Final Exam Retake/01imitationGame.js
+++ Programming Fundamentals with JavaScript/01. Programming Fundamentals Final Exam Retake/01imitationGame.js
@@ -1,38 +1,69 @@
 function imitationGame(input){
 
     let message = input.shift();
+    let arr = message.split(""); 
+    let str = "";
+    //console.log(arr)
 
     
 
     for(let i = 0; i<input.length; i++){
         let temp = input[i].split("|");
         let command = temp.shift();
+        
 
         if(command === "ChangeAll"){
 
-            let value = temp[0];
+            let substring = temp.shift();          
 
-            let num = message.includes(value);
+            let replacement = temp;
 
-            console.log(num)
+            for(let k = 0; k<arr.length ; k++){
+                let tempChar = arr[k];
+                //console.log(tempChar)
+                if(tempChar === substring){
+                    arr.splice(k, 1, replacement)
+                }
+            }
+
+            //console.log(replacement);
+
+            //console.log(value)
+
+            //console.log(num)
             
-        }
+        }else if(command === "Insert"){
+            let index = Number(temp.shift());
+            let value = temp;
 
+            arr.splice(index,0, value)
+            //console.log(value)
+        }else if(command === "Move"){
+            let firstN = Number(temp);
+
+            //let firstChar = arr.splice(0, firstN);
+
+            for(let f = 0; f<firstN; f++){
+                let tempF = arr.shift();
+                arr.push(tempF)
+            }
+
+
+            //console.log(firstChar)
+        }else if(command === "Decode"){
+
+            for(let a = 0; a < arr.length; a++){
+                let tempA = arr[a];
+                str += tempA;        
+            }
+
+            console.log(`The decrypted message is: ${str}`)
+
+        }
         
     }
-
-    let text = ""
-    let arr = message.split("");
-    for(let i = 0; i<arr.length; i++){
-        text += arr[i] 
-    }
-
-    console.log(text)
-
-    //let num = message.includes(z)
-    //console.log(num)
+    
 }
-
 imitationGame([
     'zzHe',
     'ChangeAll|z|l',
