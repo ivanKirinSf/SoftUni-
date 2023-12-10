36736999e7a91e7f4d3d474c2742aa52a15efbb3
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-12-10 19:41:46 +0200

    Update secretChat.js

--- Programming Fundamentals with JavaScript/Exam Preparation/secretChat.js
+++ Programming Fundamentals with JavaScript/Exam Preparation/secretChat.js
@@ -1,41 +1,68 @@
 function secretChat(input){
 
     let message = input.shift();
-
-    for(let i = 0; i < input.length; i++){
-        let temp = input[i].split(":|:"); 
-        let command = temp.shift(); 
+    for(let i = 0; i<input.length; i++){
+        let temp = input[i].split(":|:");
+        let command = temp.shift();
         let value = temp;
+        //console.log(value)
 
         switch(command){
-            case "ChangeAll":
+            case "ChangeAll": {
                 let substring = value.shift();
-                let replacement = value;
-                console.log(replacement)
+                let replacement = value.shift();
+                //console.log(substring)
                 if(message.includes(substring)){
-                    message = message.replace(substring, replacement)
+                    message = message.replace(substring, replacement);             
                 }
-                break;
-                case "Reverse":
-                    let substringR = value;
-                    if(message.includes(substringR)){
-                        message = message.replace(substringR, "");
-                    }                    
-                    break;                    
-        }      
-        
+                console.log(message);
+            } break;
+            case "Reverse": {
+                let substring = value.shift();
+                if(message.includes(substring)){
+                    message = message.replace(substring, "");
+                    let reversed = reverseStr(substring);
+                    message = message+=reversed;
+                    
+                }else{
+                    console.log("error")
+                }
+                console.log(message);
+            } break;
+
+            case "InsertSpace": {
+                let index = Number(value.shift());
+                let leftSide = message.substring(0,index);
+                let rightSide = message.substring(index);
+                message = leftSide + " " + rightSide;
+                console.log(message);
+            } break;            
+        }
+
+        if(command === "Reveal"){
+            console.log(`You have a new text message: ${message}`);
+
+            break;
+            
+        }
     }
 
-    console.log(message)
+    function reverseStr(str){
+        let text = str.split("").reverse().join("");
+        return text
+    }
+
+    //console.log(message)
 
 }
 
+
 secretChat([
     'Hiware?uiy',
-    //'ChangeAll:|:i:|:o',
+    'ChangeAll:|:i:|:o',
     'Reverse:|:?uoy',
     //'Reverse:|:jd',
     //'InsertSpace:|:3',
     //'InsertSpace:|:7',
     //'Reveal'
-  ])
+  ]  )
