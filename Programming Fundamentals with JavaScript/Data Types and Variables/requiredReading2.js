78320642a1c0e729aa78acc99ba1818d774a17ae
Ivan Kirin <arch.ivan.kirin@gmail.com>
2023-12-28 18:54:27 +0200

    Create requiredReading2.js

---
+++ Programming Fundamentals with JavaScript/Data Types and Variables/requiredReading2.js
@@ -0,0 +1,14 @@
+function requiredReading(pages, pagesPerHour, days){
+
+    let totalHours = pages/pagesPerHour;
+
+    let hoursPerDay = totalHours/days;
+
+    console.log(hoursPerDay)
+
+}
+
+requiredReading(432,
+    15 ,
+    4 
+     )
