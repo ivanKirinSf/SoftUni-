6561fad489e840b875bfd488ff619d129e6328e1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-01-28 21:18:01 +0200

    Update movingTarget.js

--- Programming Fundamentals with JavaScript/Mid Exam Preparation/movingTarget.js
+++ Programming Fundamentals with JavaScript/Mid Exam Preparation/movingTarget.js
@@ -2,95 +2,34 @@ function movingTarget(input){
 
     let targets = input.shift().split(" ").map(Number);
 
-    let commands = input;
+    let commands = input
 
-    for(let i = 0; i<input.length; i++){
-        let temp = input[i].split(" ");
+    for(let i = 0; i< commands.length; i++){
+        
+        let temp = commands[i].split(" ");
 
         let command = temp.shift();
 
-        switch(command){
-            case "Shoot": 
+        if(command === "Shoot"){
 
-            let shotIndex = Number(temp.shift());
+            let index = Number(temp.shift());
 
-            let shotPower = Number(temp.shift());
+            let value = Number(temp.shift());
 
-            if(shotIndex < targets.length){
+            if(index < targets.length && index >= 0){
 
-            let target = Number(targets[shotIndex]);
+               let res = targets[index] - value;
 
-            let res = target - shotPower;
+               if(res <= 0){
+                targets.splice(index,1)
+               }else {
 
-            targets[shotIndex] = res;
+                targets[index] = res
 
-            if(res <= 0){
-                targets.splice(shotIndex, 1)
+               }      
+
+            }else if(command === "Add"){
+
+                
+
             }
-
-            }           
-
-            break;
-
-            case "Add":
-
-            let addIndex = Number(temp.shift());
-
-            let addValue = Number(temp.shift());
-
-            if(addIndex < targets.length){
-                targets.splice(addIndex, 1, addValue)
-            }else{
-                console.log("Invalid placement!")
-            }
-
-            break;
-
-            case "Strike":
-
-            let strikeIndex = Number(temp.shift());
-
-            let radiusIndex = Number(temp.shift());
-
-            if(strikeIndex < targets.length){
-
-               
-            let start = strikeIndex - radiusIndex;
-
-            let range = radiusIndex*2+1;
-
-            if(start <= 0 || (range + start-1) >= targets.length-1){
-                console.log("Strike missed!");
-                continue;
-            }               
-            
-            targets.splice(start, range);
-
-            }else {
-                console.log("Strike missed!")
-            }           
-
-            break;
-
-            case "End":
-
-            console.log(targets.join("|"))
-
-
-            //console.log(addIndex)
-        }
-
-
-
-        //console.log(commands)
-    }
-
-    //console.log(targets)
-
-}
-
-movingTarget([
-"1 2 3 4 5",
-"Strike 0 1",
-"End"
-])
