97aa99bb2968ea23ad5d642194e6abdd8dbb5066
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-02-13 23:46:28 +0200

    Update theLift.js

--- Programming Fundamentals with JavaScript/Mid Exam Preparation/theLift.js
+++ Programming Fundamentals with JavaScript/Mid Exam Preparation/theLift.js
@@ -1,82 +1,77 @@
 function theLift(input){
 
-    let people = Number(input.shift());
+    let people = Number(input.shift())
 
     let lift = input.shift().split(" ").map(Number);
 
-    let isFull = true;
+    let isLiftEmpty = true;
 
-    //console.log(people)
     //console.log(lift)
 
-    for(let i = 0 ; i < lift.length; i++){
+    for(let i = 0; i < lift.length; i++){
 
-       let wagon = Number(lift[i]);
+        let wagon = Number(lift[i]);
 
-       if(people === 0 && wagon < 4){
+        let res = 4-wagon;
 
-            console.log(`The lift has empty spots!`)
-            console.log(lift.join(" "))
-            break;            
-       
-        //continue;
-       }
+        if(people > res){
 
-       if(wagon < 4){
+            lift[i] = 4
 
-        let emptySeats = 4 - wagon;
+            people = people - res
 
-        if(people - emptySeats > 0){        
-
-        people = people - emptySeats;
-
-        lift[i] = 4;
-
-       }else if(people > 0 && people < 4){
-
-        lift[i] = people;
-
-        people = 0;
-
-       }      
-       
-        } else {
-            continue;
-        }      
-
+        }else if(people <= res){
+            let sum = wagon + people;
+            lift[i] = sum
+            people = 0;
+        }        
     }
 
-    if(people > 0){
-
-        console.log(`There isn't enough space! ${people} people in a queue!`);
-        console.log(lift.join(" "));
-        
-
-    }
+    let counter = 0
 
     for(let a = 0; a < lift.length; a++){
         let tempA = Number(lift[a]);
 
-        if(tempA !== 4){
-            isFull = false;
+        if(tempA === 4){
+
+            counter += 1
+
+        }else{
             break;
         }
-    }   
-    
-    if(isFull === true && people === 0){
+
+    }
+
+    if(counter === lift.length){
+
+        isLiftEmpty = false;
+
+    }
+
+    if(isLiftEmpty === true && people === 0){
+
+        console.log(`The lift has empty spots!`)
+        console.log(lift.join(" "))
+
+    }
+
+    if(isLiftEmpty === false && people !== 0){
+
+        console.log(`There isn't enough space! ${people} people in a queue!`)
+        console.log(lift.join(" "))
+
+    }
+
+    if(isLiftEmpty === false && people === 0){
 
         console.log(lift.join(" "))
 
     }
 
-    //console.log(lift)
-
-    //console.log(people)
-
 }
 
 theLift([
-    "15",
-    "0 0 0 0 0"
-   ]     
+    "0",
+    "0 0 0"
+   ]   
    )
