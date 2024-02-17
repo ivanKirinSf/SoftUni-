8dfac2e4370d5bcce0710511855c9abaf5a054a5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-02-17 18:34:50 +0200

    Update arrayModifier.js

--- Programming Fundamentals with JavaScript/Mid Exam Preparation/arrayModifier.js
+++ Programming Fundamentals with JavaScript/Mid Exam Preparation/arrayModifier.js
@@ -10,8 +10,60 @@ function arrayModifier(input){
     
     let temp = commands[i].split(" ");
 
-    console.log(temp)
+    let command = temp.shift();
+
+    if(command === "swap"){
+
+      let index1 = Number(temp.shift());
+
+      let index2 = Number(temp.shift());
+
+      let el1 = numbers[index1]; 
+      
+      let el2 = numbers[index2];
+
+      numbers.splice(index1, 1, el2);
+
+      numbers.splice(index2, 1, el1);     
+
+      //console.log(el2)
+
+    }else if(command === "multiply"){
+
+      let index1 = Number(temp.shift());
+
+      let index2 = Number(temp.shift());
+
+      let el1 = Number(numbers[index1]);
+
+      let el2 = Number(numbers[index2]);
+
+      let res = el1*el2;
+
+      numbers.splice(index1, 1, res)
+
+    }else if(command === "decrease"){
+
+       for(let a = 0; a < numbers.length; a++){
+
+           let tempEl = Number(numbers[a]);
+
+           let res = tempEl - 1;
+           
+           numbers.splice(a, 1, res)
+           
+       }
+
+    }else if(command === "end"){
+
+      console.log(numbers.join(", "))
+
+    }
+
+    //console.log(numbers)
  }
+
+ //console.log(numbers)
 
 }
 
