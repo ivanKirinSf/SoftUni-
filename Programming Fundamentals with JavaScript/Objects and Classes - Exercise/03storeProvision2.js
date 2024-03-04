967461cc50a01971156a0b194624fb86a6fe9cc1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-03-04 20:13:24 +0200

    Update 03storeProvision2.js

--- Programming Fundamentals with JavaScript/Objects and Classes - Exercise/03storeProvision2.js
+++ Programming Fundamentals with JavaScript/Objects and Classes - Exercise/03storeProvision2.js
@@ -1,61 +1,57 @@
 function storeProvision(arr1, arr2){
 
-    let store = {};
+    let num = arr1.length;
 
-    let list = [];
-
-    for(let i = 0; i < arr1.length; i++){
-        let temp1 = arr1[i];
-        let temp2 = arr2[i];
-
-        let index1 = arr2.indexOf(temp1);
-        let index2 = arr1.indexOf(temp2);
+    let provision = [];
 
 
+    for(let i = 0; i < num; i+=2){
 
+        let product1 = arr1[i];
+        let product2 = arr2[i];
 
-        if(i % 2 === 0){
+        let index = arr1.indexOf(product2);
 
-        let index1 = arr2.indexOf(temp1);
-        let index2 = arr1.indexOf(temp2);
+        if(index !== -1){
+            let res = Number(arr2[i+1]) + Number(arr1[index+1]);
 
-           if(index1 !== -1){
-            let res = Number(arr1[i+1]) + Number(arr2[index1+1]);
-            store.product = temp1;
-            store.quantity = res;
-           }else if(index1 === -1){
-            store.product = temp1;
-            store.quantity = Number(arr1[i+1])
-           }
+            res = "" + res
 
-           list.push(store)
+            arr1.splice((index+1), 1, res)
 
-           store = {};
+        }else if(index === -1){
 
-           //console.log(`${store.product} -> ${store.quantity}`)
-           
-           if(index2 !== -1){
-            let res = Number(arr2[i+1]) + Number(arr1[index2+1]);
-            store.product = temp2;
-            store.quantity = res;
-           }else if(index2 === -1){
-            store.product = temp2;
-            store.quantity = Number(arr2[i+1])
-           }
+            arr1.push(product2);
+            arr1.push(arr2[i+1]);
 
-           list.push(store)
-            
         }
 
-        //console.log(`${store.product} -> ${store.quantity}`)
     }
 
+    for(let k = 0; k < arr1.length; k+=2){
 
-    //console.log(list)
+        let temp = arr1[k];
 
-    for(let el of list){
-        console.log(`${el.product} -> ${el.quantity}`)
-    }
-    //console.log(store)
+        provision.product = temp;
+        provision.quantity = arr1[k+1];
+
+        console.log(`${provision[`product`]} -> ${provision[`quantity`]}`)
+
+    }   
 
 }
+
+  
+
+// push two arrays into third array(or modify the first one)
+// create object and place properties
+// push object into final arr
+// print final arr
+
+storeProvision([
+    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
+    ],
+    [
+    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    ]        
+    )
