0e1a79cad61d26418d67f10c30c4d102f44319e0
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-03-26 22:56:16 +0200

    Update theImitationGame.js

--- Programming Fundamentals with JavaScript/Programming Fundamentals Final Exam/theImitationGame.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Final Exam/theImitationGame.js
@@ -1,71 +1,76 @@
-function theImitationGame(input){
+function imitationGame(input){
 
     let message = input.shift().split("");
 
-    //console.log(message);
-
-    //let strings = input;
-
-    for(let i = 0; i<input.length; i++){
+    for(let i = 0; i < input.length; i++){
         let temp = input[i].split("|");
-        let command = temp.shift()
 
-        if(command === "Decode"){
-            console.log(`The decrypted message is: ${message.join("")}`)
+        let command = temp.shift();
+
+        if(command === "Move"){
+
+            let num = Number(temp.shift());
+
+            index = 0;
+            while(index < num){
+                let first = message.shift();
+
+                message.push(first);
+
+                index++;
+            }           
+
+        }else if(command === "Insert"){
+
+            let index = Number(temp.shift());
+
+            let value = temp.shift().split("").reverse();
+
+            for(let el of value){
+
+                message.splice(index, 0, el);
+
+            }
+
+            //console.log(value);
+
         }else if(command === "ChangeAll"){
 
             let substring = temp.shift();
 
             let replacement = temp.shift();
 
+
             let index = message.indexOf(substring);
-            
+
             while(index !== -1){
+
+                message.splice(index, 1, replacement)
+
+                    index = message.indexOf(substring);
+
                 
-                message.splice(index, 1, replacement);
-                index = message.indexOf(substring)
-            };
-            //console.log(replacement)
-        }else if(command === "Insert"){
-
-            let index = Number(temp.shift());
-
-            let value = temp.shift();
-
-            message.splice(index, 0, value);
-
-        }else if(command === "Move"){
-
-            let indexMove = Number(temp.shift());
-
-            let index = 0;
-
-            while( index < indexMove ){
-
-                let firstChar = message.shift();
-                
-                message.push(firstChar);
-
-                index++;
-
             }
 
+            //console.log(substring)
+
+        }else if(command === "Decode"){
+
+            console.log(`The decrypted message is: ${message.join("")}`)
         }
 
-        //console.log(temp)
-
+        //console.log(command)
     }
 
     //console.log(message)
 
 }
 
-theImitationGame([
-
-    'zzHe',
-    'ChangeAll|z|l',
-    'Insert|2|o',
+imitationGame([
+    'owyouh',
+    'Move|2',
     'Move|3',
+    'Insert|3|are',
+    'Insert|9|?',
     'Decode'
-
-  ])
+])
