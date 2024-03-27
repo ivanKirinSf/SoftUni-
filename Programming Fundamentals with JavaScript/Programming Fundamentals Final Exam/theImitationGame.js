e6443314988f12ccea2cb2d08625bd22eade3c90
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-03-27 22:00:58 +0200

    Update theImitationGame.js

--- Programming Fundamentals with JavaScript/Programming Fundamentals Final Exam/theImitationGame.js
+++ Programming Fundamentals with JavaScript/Programming Fundamentals Final Exam/theImitationGame.js
@@ -3,22 +3,24 @@ function imitationGame(input){
     let message = input.shift().split("");
 
     for(let i = 0; i < input.length; i++){
+
         let temp = input[i].split("|");
 
         let command = temp.shift();
 
         if(command === "Move"){
-
             let num = Number(temp.shift());
 
-            index = 0;
+            let index = 0;
             while(index < num){
+
                 let first = message.shift();
 
-                message.push(first);
-
+                message.push(first);    
+                
                 index++;
-            }           
+
+            }
 
         }else if(command === "Insert"){
 
@@ -32,7 +34,7 @@ function imitationGame(input){
 
             }
 
-            //console.log(value);
+            //console.log(value)           
 
         }else if(command === "ChangeAll"){
 
@@ -40,37 +42,38 @@ function imitationGame(input){
 
             let replacement = temp.shift();
 
-
             let index = message.indexOf(substring);
 
             while(index !== -1){
 
-                message.splice(index, 1, replacement)
-
-                    index = message.indexOf(substring);
-
                 
-            }
 
-            //console.log(substring)
+                message.splice(index, 1, replacement);
+
+                index = message.indexOf(substring);
+            }
 
         }else if(command === "Decode"){
 
             console.log(`The decrypted message is: ${message.join("")}`)
+
+            
+
         }
 
-        //console.log(command)
-    }
+        //console.log(command)        
+
+    }  
 
     //console.log(message)
-
 }
 
 imitationGame([
+
     'owyouh',
     'Move|2',
     'Move|3',
     'Insert|3|are',
     'Insert|9|?',
     'Decode'
-])
+  ])
