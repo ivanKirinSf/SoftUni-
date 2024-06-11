0c1c6d1e57b34fe1b0b5c250508969c39633dc94
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-06-11 22:17:04 +0300

    Update 05cardGame.js

--- Programming Fundamentals with JavaScript/Associative Arrays - Exercise/05cardGame.js
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/05cardGame.js
@@ -1,116 +1,28 @@
-function cardGame(input) {
+function cardGame(input){
 
-    //let list = input.split(", ");
+   let players = new Map();
 
-    let players = {};
+   for(let el of input){
 
-    let personValue = [];
+    let temp = el.split(": ");
 
-    let value = 0;
+    let name = temp.shift();
 
-    let person = "";
+    let cards = temp[0];
 
-    for (let temp of input) {
+    if(!players.has(name)){
 
-        let data = temp.split(": ");
-
-        person = data.shift();
-
-        let cardsList = data.shift().split(", ");
-
-        for (let card of cardsList) {
-
-            card = card.split("");
-
-            let type = card.pop();
-
-            let length = card.length;
-
-            let power = "";
-
-            let typeCode = "";
-
-            if(length === 2) {
-
-                power = 10;
-
-            }else{
-
-                power = card.shift()
-
-                if(power === "J"){
-
-                   power = 11;
-
-                }else if(power === "Q"){
-
-                    power = 12;
- 
-                 }else if(power === "K"){
-
-                    power = 13;
- 
-                 }else if(power === "A"){
-
-                    power = 14;
- 
-                 }else{
-
-                    power = Number(power);
-
-                 }
-            }  
-            
-            if(type === "S"){
-
-               type = 4;
-
-            }else if(type === "H"){
-
-                type = 3;
-
-            }else if(type === "D"){
-
-                type = 2;
-                
-            }else if(type === "C"){
-
-                type = 1;
-                
-            }
-
-            let currentValue = type*power;
-
-            personValue.push(currentValue);           
-
-        }       
-
-        //console.log(value)
-        //console.log(cardsList)
-
-        for(let num of personValue){
-
-            value += num;
-    
-            players[person] = value;
-    
-        }
-    }    
-
-    console.table(players);
-
-    //let entries = Object.entries(players);
-
-
+        players.set(name, new Set())
+    }
+   }
 
 }
 
-cardGame(
-    ['Peter: 2C, 4H, 9H, AS, QS',
-    'Tomas: 3H',
-    'Andrea: QH',
-    'Tomas: 6H',
-    'Andrea: QH',
-    'Peter: JD'
-    ]
-)
+cardGame([
+    'Peter: 2C, 4H, 9H, AS, QS',
+    'Tomas: 3H, 10S, JC, KD, 5S, 10S',
+    'Andrea: QH, QC, QS, QD',
+    'Tomas: 6H, 7S, KC, KD, 5S, 10C',
+    'Andrea: QH, QC, JS, JD, JC',
+    'Peter: JD, JD, JD, JD, JD, JD'
+    ])
