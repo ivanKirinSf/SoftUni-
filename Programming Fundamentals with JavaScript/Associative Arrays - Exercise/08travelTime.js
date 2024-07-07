b6d6f158b2056d18f508b023161dd821522b7ff9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-07-07 21:48:41 +0300

    Update 08travelTime.js

--- Programming Fundamentals with JavaScript/Associative Arrays - Exercise/08travelTime.js
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/08travelTime.js
@@ -2,53 +2,48 @@ function travelTime(input){
 
 let countries = {};
 
-for(let el of input){
+for(let row of input){
 
-  let temp = el.split(" > ");
+  let temp = row.split(" > ");
 
-  let state = temp[0];
-  let town = temp[1];
-  let cost = temp[2];
+  let country = temp[0];
+  let city = temp[1];
+  let price = temp[2];
 
-  if(!countries.hasOwnProperty(state)){
-
-    countries[state] = {};
+  if(!countries.hasOwnProperty(country)){
+    countries[country] = {};
   }
 
-  if(!countries[state].hasOwnProperty(town)){
-
-    countries[state][town] = cost;
-
+  if(!countries[country].hasOwnProperty(city)){
+    countries[country][city] = price;
   }
 
-  if(countries[state][town] > cost){
+  if(countries[country][city] > price){
 
-    countries[state][town] = cost;
-
-  }
-
-  let keys = Object.keys(countries);
-
-  keys.sort((a,b) => a.localeCompare(b));
-
-  for(let item of keys){
-
-    let sortedCities = Object.entries(countries[item]);
-    sortedCities.sort((a,b) => a[1] - b[1]);
-
-    let result = [];
-
-    for(let row of sortedCities){
-
-       result.push(row.join(" -> "));
-
-       console.log(`${item} -> ${result.join(" ")}`)
-
-    }
+    countries[country][city] = price;
 
   }
-
 }
+
+let keys = Object.keys(countries);
+keys.sort((a,b) => a.localeCompare(b));
+
+for(let item of keys){
+  let sortedCities = Object.entries(countries[item]);
+  sortedCities.sort((a,b) => a[1] - b[1]);
+
+  let result = [];
+
+  for(let city of sortedCities){
+
+      result.push(city.join(" -> "));
+      //console.log(`${item} -> ${result.join(" ")}`)
+  }
+
+  console.log(`${item} -> ${result.join(" ")}`)
+}
+
+
 }
 
 travelTime([
