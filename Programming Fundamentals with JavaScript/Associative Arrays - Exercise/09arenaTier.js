dfb87fe2478685b06f590b0f531ec85bd6915b97
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-07-15 21:30:24 +0300

    Update 09arenaTier.js

--- Programming Fundamentals with JavaScript/Associative Arrays - Exercise/09arenaTier.js
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/09arenaTier.js
@@ -3,42 +3,26 @@ function arenaTier(input){
 let gladiatorsPool = new Map();
 
 let index = 0;
-
 let command = input[index];
-
-index ++;
+index ++; 
 
 while(command !== "Ave Cesar"){
-    action = command.split(" -> ");
-    let name = action.shift();
-    let technique = action.shift();
-    let skill = action.shift();
+    let action = command.split(" -> ");
 
     if(action.length === 3){
+
+        let [name, technique, skill] = action;
+
         if(!gladiatorsPool.has(name)){
             gladiatorsPool.set(name, new Map)
+        }else if(gladiatorsPool.has(name).gladiatorsPool.get(technique)){
+          
+
         }
+
     }else {
-        action = command.split(" vs "){
-            let nameA = action.shift();
-            let nameB = action.shift();
-        }
-    }    
-
-    command = input[index];
-    index++;
+        action = command.split(" vs ")
+    }
 }
 
 }
-
-arenaTier([
-    'Peter -> Duck -> 400',
-    'Julius -> Shield -> 150',
-    'Gladius -> Heal -> 200',
-    'Gladius -> Support -> 250',
-    'Gladius -> Shield -> 250',
-    'Peter vs Gladius',
-    'Gladius vs Julius',
-    'Gladius vs Maximilian',
-    'Ave Cesar'
-    ])
