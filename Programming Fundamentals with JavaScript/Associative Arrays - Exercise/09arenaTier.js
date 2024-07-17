56c3ab787ae35a10ea52bc3d6266a04adb9f4e61
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-07-17 22:24:12 +0300

    Update 09arenaTier.js

--- Programming Fundamentals with JavaScript/Associative Arrays - Exercise/09arenaTier.js
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/09arenaTier.js
@@ -3,10 +3,13 @@ function arenaTier(input){
 let gladiatorsPool = new Map();
 
 let index = 0;
-let command = input[index];
-index ++; 
 
-while(command !== "Ave Cesar"){
+let command = input[index];
+
+index++;
+
+while(command !== " Ave Cesar "){
+
     let action = command.split(" -> ");
 
     if(action.length === 3){
@@ -14,15 +17,36 @@ while(command !== "Ave Cesar"){
         let [name, technique, skill] = action;
 
         if(!gladiatorsPool.has(name)){
-            gladiatorsPool.set(name, new Map)
-        }else if(gladiatorsPool.has(name).gladiatorsPool.get(technique)){
-          
-
+            gladiatorsPool.set(name, new Map());
         }
+        if(!gladiatorsPool.get(name).has(technique) || 
+            gladiatorsPool.get(name).has(technique) && 
+            gladiatorsPool.get(name).get(technique) < skill){
+            gladiatorsPool.get(name).set(technique, Number(skill));
+            }
 
-    }else {
-        action = command.split(" vs ")
+    }else{
+
+        action = command.split(" vs ");
     }
-}
+
+    command = input[index];
+    index ++; 
 
 }
+
+console.table(gladiatorsPool);
+
+}
+
+arenaTier([
+    'Peter -> Duck -> 400',
+    'Julius -> Shield -> 150',
+    'Gladius -> Heal -> 200',
+    'Gladius -> Support -> 250',
+    'Gladius -> Shield -> 250',
+    'Peter vs Gladius',
+    'Gladius vs Julius',
+    'Gladius vs Maximilian',
+    'Ave Cesar'
+    ])
