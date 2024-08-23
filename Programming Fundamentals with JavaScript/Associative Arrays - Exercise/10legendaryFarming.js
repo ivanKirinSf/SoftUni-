aa06eb782f23d788c7a1063e5a3f7c7e236ef2e9
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-08-23 23:19:57 +0300

    Create 10legendaryFarming.js

---
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/10legendaryFarming.js
@@ -0,0 +1,17 @@
+function legendaryFarming(input){
+    
+let materials = input.split(" ");
+
+let quantity = {};
+
+for(let i = 0; i < input.length; i+=2){
+
+    let temp = input[i];
+    let quantity = input[i+1];
+    console.log(temp);
+
+}
+
+}
+
+legendaryFarming('3 Motes 5 stones 5 Shards 6 leathers 255 fragments 7 Shards')
