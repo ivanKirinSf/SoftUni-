e66857b8031cf08b8a2fb29f47dbb63e92bd5539
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-08-25 12:42:09 +0300

    Update 10legendaryFarming.js

--- Programming Fundamentals with JavaScript/Associative Arrays - Exercise/10legendaryFarming.js
+++ Programming Fundamentals with JavaScript/Associative Arrays - Exercise/10legendaryFarming.js
@@ -2,25 +2,30 @@ function legendaryFarming(input){
     
 let arr = input.split(" ");
 
-let keyMat = { shadowmourne : 0 , valanyr : 0 , dragonwrath : 0 };
+let keyMat = { shards : 0, fragments : 0, motes : 0 }
 
-let junkMat = {};
+let legendaries = { shards : "Shadowmourne", fragments : "Valanyr", motes : "Dragonwrath" }
 
-for(let i = 0 ; i < arr.length; i += 2){
-    
-    let material = arr[i + 1].toLowerCase();
+for(let i = 0; i < arr.length; i+=2){
+
+    let mat = arr[i+1].toLowerCase();
 
     let quantity = Number(arr[i]);
-    
-    //console.log(material);
-    //console.log(quantity);
 
-    if(material in keyMat){
+    if(mat in keyMat){
 
-        keyMat[material] += quantity;
+        keyMat[mat] += quantity;
 
-        console.table(keyMat)
+        if(keyMat[mat] >= 250){
+
+            let legendary = legendaries[mat];
+            console.log(`${legendary} obtained!`);
+            keyMat[mat] -= 250;
+            break;
+        }
     }
+
+    console.log(quantity);
 
 }
 
