3021abf2bae0c8a3f085516cc481532bd1c48434
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-09-14 13:56:40 +0300

    Create 02substring.js

---
+++ Programming Fundamentals with JavaScript/Text Processing/02substring.js
@@ -0,0 +1,11 @@
+function substring(txt, start, count){
+
+    let str = txt;
+
+    let newStr = str.substring(start, start+count);
+
+    console.log( newStr )
+
+}
+
+substring('ASentence', 1, 8)
