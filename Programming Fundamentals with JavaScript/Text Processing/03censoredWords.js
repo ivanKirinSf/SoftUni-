fcb9602e9277021c8ebdf7755d2f46ae97c89ad7
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-09-15 19:55:09 +0300

    Create 03censoredWords.js

---
+++ Programming Fundamentals with JavaScript/Text Processing/03censoredWords.js
@@ -0,0 +1,13 @@
+function censoredWords(text, word){
+
+  while(text.includes(word)){
+
+    text = text.replace(word, "*".repeat(word.length));
+    
+  }
+
+  console.log(text)
+
+}
+
+censoredWords('A small sentence with some words', 'small')
