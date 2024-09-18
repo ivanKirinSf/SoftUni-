30d0c157e8d6a04c3a38070f9321083093c41087
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-09-18 21:30:28 +0300

    Update 01revealWords.js

--- Programming Fundamentals with JavaScript/Text Processing - exercise/01revealWords.js
+++ Programming Fundamentals with JavaScript/Text Processing - exercise/01revealWords.js
@@ -4,6 +4,8 @@ function revealWords(word, txt){
 
     let counter = 0;
 
+    let stars = "";
+
     for(let i = 0; i < arr.length; i++){
 
         for(let ch of arr[i]){
@@ -16,12 +18,22 @@ function revealWords(word, txt){
                  break;
             }
         }
+        
     }
 
-    //console.log(arr)
+    if(counter === word.length){
+
+        stars = "*".repeat(counter);
+
+        //console.log(stars)      
+
+    }
+
+    let replacedText = txt.replace(stars, word)
+
+    console.log(replacedText);
 
 }
 
 revealWords('great',
-'softuni is ***** place for learning new programming languages'
-)
+'softuni is ***** place for learning new programming languages')
