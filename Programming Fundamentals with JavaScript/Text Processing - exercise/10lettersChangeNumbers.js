e91c9787b6968d84a51d243046f3bd1a28ca8ea5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-10-12 18:44:46 +0300

    Update 10lettersChangeNumbers.js

--- Programming Fundamentals with JavaScript/Text Processing - exercise/10lettersChangeNumbers.js
+++ Programming Fundamentals with JavaScript/Text Processing - exercise/10lettersChangeNumbers.js
@@ -1,5 +1,7 @@
 function lettersChangeNumbers(txt){
 
+    
+
     let arr = txt.split(" ");
 
     let num = ""
@@ -7,8 +9,11 @@ function lettersChangeNumbers(txt){
     let alphabetUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     let alphabetLow = "abcdefghijklmnopqrstuvwxyz";
-    
 
+    let res = 0;
+
+    let arrRes = [];
+    
     for(let i = 0; i < arr.length; i++){
 
         let temp = arr[i].split("");        
@@ -19,41 +24,57 @@ function lettersChangeNumbers(txt){
 
         let firstCode = firstLetter.charCodeAt();
 
-        num = Number(temp.join(""));      
+        let lastCode = lastLetter.charCodeAt();
 
-        
+        num = Number(temp.join(""));        
 
         if(firstCode >= 65 && firstCode <= 90){
 
             let firstPos = alphabetUp.indexOf(firstLetter) + 1;
          
-            let res = num / firstPos;
+            res = num / firstPos;
 
-            console.log(res)
+            //console.log(res);
+
+        }else if(firstCode >= 97 && firstCode <= 122){
+
+            let firstPos = alphabetLow.indexOf(firstLetter) + 1;
+            
+            res = num * firstPos; 
+            
+            //console.log(res);
+            
+        }
+
+        if(lastCode  >= 65 && lastCode <= 90){
+
+            let lastPos = alphabetUp.indexOf(lastLetter) + 1;
+
+            res = res - lastPos;
+
+        }else if(lastCode  >= 97 && lastCode <= 122){
+
+            let lastPos = alphabetLow.indexOf(lastLetter) + 1;
+
+            res = res + lastPos;
 
         }
 
-        //let firstPos = firstLetter.charCodeAt();
-
-        //let lastLetter = temp.pop();
-
-        //let lastCode = lastLetter.charCodeAt();
-
-        //num = Number(temp.join(""));
-
-
-
-        //console.log(firstCode);
+        arrRes.push(res);
 
     }
 
     //console.log(num);
 
-    //console.log(arr);
+    let result = 0;
 
+    for(let el of arrRes){
 
+        result += el;
+    }
 
+    console.log(result.toFixed(2));
 
 }
 
-lettersChangeNumbers( 'B12b' )
+lettersChangeNumbers( 'P34562Z q2576f   H456z' )
