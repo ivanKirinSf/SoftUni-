554a05a33dcb97a9ca8e33ce7c98923fa9934a96
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-04 21:21:47 +0200

    Create matchDates.js

---
+++ Programming Fundamentals with JavaScript/Regular Expressions/matchDates.js
@@ -0,0 +1,19 @@
+function matchDates(input){
+
+    let patern = \/b /b\g;
+
+    while((date = patern.exec(input)) !== null){
+
+        let day = date.groups["day"];
+
+        let month = date.groups["month"];
+
+        let year = date.groups["year"];
+
+        console.log(`${day}, ${month}, ${year}`)
+    }
+
+
+}
+
+matchDates(['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016'])
