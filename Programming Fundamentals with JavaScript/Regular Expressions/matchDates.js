a2adb6c4a40dbbe01fde3e976198bf0963741b93
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-08 20:56:41 +0200

    Update matchDates.js

--- Programming Fundamentals with JavaScript/Regular Expressions/matchDates.js
+++ Programming Fundamentals with JavaScript/Regular Expressions/matchDates.js
@@ -1,20 +1,17 @@
-function matchDates(input){
+function matchDates(dates){
 
-let pattern = /\bd{2}[.-/][A-Z][a-z]\2\[.-/]d{4}\b/g;
+let pattern = /\b(?<day>\d{2})([.\/])(?<month>[A-Z][a-z]\2)\2(?<year>\d{4})\b/g;
 
-let validDates = [];
+while((validDate = pattern.exec(dates)) !== null){
 
-let dates = null;
+   let day = validDate.groups['day'];
+   let month = validDate.groups['month'];
+   let year = validDate.groups['year'];
 
-while((dates = pattern.exec(input)) !== null){
-
-   let day = pattern[`day`];
-   let month = pattern[`month`];
-   let year = pattern[`year`];
-
+   console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+   
 }
 
-console.log(validDates.join(" "))
 }
 
 matchDates(['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016'])
