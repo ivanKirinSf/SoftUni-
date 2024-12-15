78aef258eebc88cf589921fdd6b75f52e804e00d
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-15 20:30:08 +0200

    Create 02race.js

---
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/02race.js
@@ -0,0 +1,28 @@
+function race(input){
+
+    let pattern = /[A-Z]+[a-z]+[0-9]/g;
+
+    let racersList = input.shift();
+
+    let racersDate = [];
+
+    for(let line of input){
+
+        if(pattern.test(line)){
+
+                        
+
+        }
+
+    }
+        
+}
+
+race(['George, Peter, Bill, Tom',
+    'G4e@55or%6g6!68e!!@ ',
+    'R1@!3a$y4456@',
+    'B5@i@#123ll',
+    'G@e54o$r6ge#',
+    '7P%et^#e5346r',
+    'T$o553m&6',
+    'end of race'])
