7cba982a90670a3cfc7daa673b35fa056cc5e7d1
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-24 13:53:19 +0200

    Update 01furniture.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/01furniture.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/01furniture.js
@@ -1,26 +1,27 @@
-function furniture(input){
+function solve(input){
 
- let pattern = /(?<product>[A-Za-z])<<(?<price>[0-9.]+)!(?<quantity>\d+)/g;
+    let regex = />>(?<product>[A-Z]+[A-Z][a-z]+)<<(?<price>\d+)!(?<quantity>d\+)/g;
 
- for(let line of input){
-    if(input[line] === 'Purchase'){
-        break;
+    for(let line of input){
+
+        if(line === "Purchase"){
+            break;
+        }
+
+        if(regex.test(line)){
+
+            
+        }
     }
 
-    if(pattern.test(line)){
-
-        let product = pattern.exec(line);
-    }
- }
 
 }
 
-furniture(['>>Laptop<<312.2323!3',
-'>>TV<<300.21314!5',
-'>Invalid<<!5',
-'>>TV<<300.21314!20',
-'>>Invalid<!5',
-'>>TV<<30.21314!5',
-'>>Invalid<<!!5',
-'Purchase']
-)
+solve(['>>Laptop<<312.2323!3',
+    '>>TV<<300.21314!5',
+    '>Invalid<<!5',
+    '>>TV<<300.21314!20',
+    '>>Invalid<!5',
+    '>>TV<<30.21314!5',
+    '>>Invalid<<!!5',
+    'Purchase'])
