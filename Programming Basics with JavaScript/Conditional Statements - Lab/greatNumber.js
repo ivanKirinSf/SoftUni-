44bb86de6d0e2cae8a561c457d735d14e15cda64
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-30 20:01:20 +0200

    Create greatNumber.js

---
+++ Programming Basics with JavaScript/Conditional Statements - Lab/greatNumber.js
@@ -0,0 +1,11 @@
+function greatNumber(input) {
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    if (num1 < num2) {
+        console.log(num2)
+    } else {
+        console.log(num1)
+    }
+}
+
+greatNumber(["5", "3"])
