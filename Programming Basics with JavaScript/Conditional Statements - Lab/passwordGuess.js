c4e89e65f1bbb844b19b03d62c17cb06c2ab0cd6
Ivan Kirin <arch.ivan.kirin@gmail.com>
2024-12-30 20:02:02 +0200

    Create passwordGuess.js

---
+++ Programming Basics with JavaScript/Conditional Statements - Lab/passwordGuess.js
@@ -0,0 +1,12 @@
+function passwordGuess(input) {
+    let word = input[0];
+    if (word === "s3cr3t!P@ssw0rd"){
+        console.log("Welcome")
+    } else {
+        console.log("Wrong password!")
+    }
+
+}
+
+passwordGuess(["qwerty"])
+passwordGuess(["s3cr3t!P@ssw0rd"])
