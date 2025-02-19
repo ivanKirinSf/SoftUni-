6135f6f4a7bba2f0b527fe672070711dadcb514d
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-02-19 21:16:52 +0200

    Update 03softUniBarIncome.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/03softUniBarIncome.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/03softUniBarIncome.js
@@ -1,6 +1,6 @@
 function softUniBarIncome(input){
 
-    let patternName = /%(?<name>[A-Z][a-z]+)%<(?<product>\w+)>\|(?<count>\d+)\|(?<price>[0-9]).$/g;
+    let patternName = /%(?<name>[A-Z][a-z])%[|$%.]*<(?<product>\w+)>[|$%.]*\|(?<count>)\|(?<price>[0-9]+\.?\*)/g;
 
     totalPrice = 0;
 
