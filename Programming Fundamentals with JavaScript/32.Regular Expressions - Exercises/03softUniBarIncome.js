47e2d2926dcf4885f80e07da58512138b8a26034
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-05 21:34:36 +0200

    Update 03softUniBarIncome.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/03softUniBarIncome.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/03softUniBarIncome.js
@@ -2,35 +2,40 @@ function softUniBarIncome(input){
 
     let pattern = /%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*(?<price>[0-9]+\.?\d*)\$/;
 
-  totalPrice = 0;
+    let totalPrice = 0;
 
-  for(let line of input){
+    for(let line of input){
+        
+        let match = [];
 
-    let match = [];
+       if(line === "end of shift"){
 
-    if(line === "end of shift"){
-        break;
-    }
+       break;
 
-    if(pattern.test(line)){
+       }
+
+       if(pattern.test(line)){
 
         match = line.match(pattern);
-
-        let price = Number(match.groups.price)*Number(match.groups.count);
+        
+        let price = Number(match.groups.count) * Number(match.groups.price);
 
         totalPrice += price;
 
         console.log(`${match.groups.name}: ${match.groups.product} - ${price.toFixed(2)}`);
-    }
-  }
 
-  console.log(`Total income: ${totalPrice.toFixed(2)}`)
+       }
+
+    }
+
+    console.log(`Total income: ${totalPrice.toFixed(2)}`)
  
 }
 
 softUniBarIncome([
-    '%George%<Croissant>|2|10.3$',
-    '%Peter%<Gum>|1|1.3$',
-    '%Maria%<Cola>|1|2.4$',
+    '%InvalidName%<Croissant>|2|10.3$',
+    '%Peter%<Gum>1.3$',
+    '%Maria%<Cola>|1|2.4',
+    '%Valid%<Valid>valid|10|valid20$',
     'end of shift'
-])
+    ])
