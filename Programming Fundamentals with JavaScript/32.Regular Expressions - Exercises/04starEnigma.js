066402cc89e20d098262b67d0256e7f68da5ff0f
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-08 20:06:24 +0200

    Update 04starEnigma.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
@@ -1,34 +1,46 @@
 function starEnigma(input){
 
-    let num = input.shift();
+    let numOfmess = input.shift();
+
+    let decryptionKey = 0;
+
+    let encryptedMessage = "";
+    //console.log(input)
 
     for(let el of input){
 
-        let message = el.split("");
+        let encMessage = el;
 
-        let countingMess = el.toLowerCase().split("");
+        let caseInsensitiveMess = encMessage.toLowerCase().split("");
 
-        let counter = 0;
-
-        for(let char of countingMess){
+        for(let char of caseInsensitiveMess){
 
             if(char === "s"){
-                counter += 1
+                decryptionKey += 1;
+            }else if(char === "t"){
+                decryptionKey += 1;
+            }else if(char === "a"){
+                decryptionKey += 1;
+            }else if(char === "r"){
+                decryptionKey += 1;
             }
-            if(char === "t"){
-                counter += 1
-            }
-            if(char === "a"){
-                counter += 1
-            }
-            if(char === "r"){
-                counter += 1
-            }
+        }
 
+        for(let char2 of caseInsensitiveMess){
+
+            let value = char2.charCodeAt();
+
+            let res = value - decryptionKey;
+
+            let enChar = String.fromCharCode(res)
+
+            encryptedMessage += enChar;
 
         }
 
-        console.log(counter)
+        console.log(encryptedMessage)
+
+        //console.log(caseInsensitiveMess)
     }
 
 }
@@ -36,6 +48,6 @@ function starEnigma(input){
 starEnigma([
     '3',
     "tt(''DGsvywgerx>6444444444%H%1B9444",
-    'GQhrr|A977777(H(TTTT',
-    'EHfsytsnhf?8555&I&2C9555SR'
+    //'GQhrr|A977777(H(TTTT',
+    //'EHfsytsnhf?8555&I&2C9555SR'
 ])
