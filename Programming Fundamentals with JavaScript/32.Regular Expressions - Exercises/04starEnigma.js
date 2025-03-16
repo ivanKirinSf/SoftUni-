ab31a4b102f1065000110eb11c0c326e74fb655b
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-16 14:54:35 +0200

    Update 04starEnigma.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
@@ -1,16 +1,33 @@
-function starEnigma(input) {
-    let messageCount = Number(input.shift());
-    let starCountPattern = /[STARstar]/g;
-    let attackedPlanetsList = new Set();
-    let destroyedPlanetsList = new Set();
-    let planetPattern = /[^@\-!:>\s]*@(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)[^@\-!:>\s]*!(?<type>[A|D])![^@\-!:>\s]*\->(?<count>\d+)/;
-    
-    
-    for (const line of input) {
-        let starCount = 0;
-        let match = starCountPattern.exec(line);
- 
-        while (match !== null) {
-            starCount ++;
-            match = starCountPattern.exec(line);
+function starEnigma(input){
+
+let num = input.shift();
+
+let patternCode = /[STARstar]+/;
+
+let code = 0;
+
+let pattern = /@(?<name>[A-Z][a-z]*)[^@!:>]*:(?<population>[0-9]*)[^@!:>]*!(?<type>[AD]+)!->(?<count>[0-9]+)/;
+
+for(let line of input){
+    if(patternCode.test(line)){
+
+        let match = line.match(patternCode);
+
+        while(match !== null){
+
+            code++
+
         }
+    }
+}
+
+
+
+}
+
+starEnigma([
+    '3',
+    "tt(''DGsvywgerx>6444444444%H%1B9444",
+    //'GQhrr|A977777(H(TTTT',
+    //'EHfsytsnhf?8555&I&2C9555SR'
+])
