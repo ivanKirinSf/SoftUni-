386c2c0696128b82ce3e92d4805d5eec8feb0270
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-19 22:03:16 +0200

    Update 04starEnigma.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
@@ -2,33 +2,96 @@ function starEnigma(input){
 
 let num = input.shift();
 
-let patternStar = /[STARstar]/g;
+let patterStar = /[STARstar]/g;
 
 let pattern = /[^@\-!:>\s]*@(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)[^@\-!:>\s]*!(?<type>[A|D])![^@\-!:>\s]*\->(?<count>\d+)/;
 
-for(let line of input){
+let decMessage = "";
 
+let attackedPlanetsList = new Set();
+
+let destroyedPlanetsList = new Set();
+
+for(let line of input){
+    
  let counter = 0;
- 
- let match = line.match(patternStar);
+
+ let match = patterStar.exec(line);
 
  while(match !== null){
-
     counter ++;
 
-    match = line.match(patternStar);
+    match = patterStar.exec(line);
  }
 
- console.log(counter)
+ for(let char of line){
+
+    let value = char.charCodeAt();
+
+    let result = value - counter;
+
+    let character = String.fromCharCode(result);
+
+    decMessage += character;
+
+ } 
 
 }
+
+if(pattern.test(decMessage)){
+
+    let match = decMessage.match(pattern);
+ 
+    if(match.groups.type === "A"){
+ 
+     attackedPlanetsList.add(match.groups.name)
+ 
+    }
+ 
+    if(match.groups.type === "D"){
+ 
+     destroyedPlanetsList.add(match.groups.name)
+ 
+    }
+
+}
+
+if(attackedPlanetsList.size){
+
+    console.log(`Attacked planets: ${attackedPlanetsList.size}`);
+
+    let sortedAttacked = attackedPlanetsList.sort((a, b) => a.localeCompare(b));
+
+for(let planet of sortedAttacked){
+
+    console.log(`-> ${planet[0]}`)
+}
+
+}
+
+if(destroyedPlanetsList.size){
+
+    console.log(`Destroyed planets: ${destroyedPlanetsList.size}`);
+
+    let sortedDestroyed = destroyedPlanetsList.sort((a, b) => a.localeCompare(b));
+
+    for(let planet of destroyedPlanetsList){
+
+        console.log(`-> ${planet[0]}`)
+    }
+
+
+}
+
+
+
 
 }
 
 starEnigma(
     ['3',
         "tt(''DGsvywgerx>6444444444%H%1B9444",
-        //'GQhrr|A977777(H(TTTT',
-        //'EHfsytsnhf?8555&I&2C9555SR'
+        'GQhrr|A977777(H(TTTT',
+        'EHfsytsnhf?8555&I&2C9555SR'
         ]
     )
