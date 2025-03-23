3c97f5f4b2e91fa359bef3a4ee3588d5ada551a5
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-23 19:47:53 +0200

    Update 04starEnigma.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
@@ -8,7 +8,7 @@ let pattern = /[^@\-!:>\s]*@(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)[^@\-
 
 let counter = 0;
 
-let newMes = "";
+let newMess = "";
 
 let attackedPlanetsList = new Set();
 
@@ -16,63 +16,74 @@ let destroyedPlanetsList = new Set();
 
 for(let line of input){
 
-   counter = 0;
+    let match = starCode.exec(line);
 
-   let match = starCode.exec(line);
+    while(match !== null){
 
-   while(match !== null){
+        counter ++;
+        
+        match = starCode.exec(line);
 
-    counter ++;
+    }
 
-    match = starCode.exec(line);
+    for(let ch of line){
 
-   }
+        let value = ch.charCodeAt();
 
-   for(let ch of line){
+        let res = value - counter;
 
-    let value = ch.charCodeAt();
+        let char = String.fromCharCode(res)
 
-    let res = value - counter;
-
-    let character = String.fromCharCode(res);
-
-    newMes += character;
-    
-   }   
-   
+        newMess += char;
+    }
 }
 
-if(pattern.exec(newMes)){
+//console.log(newMess);
 
-    let match = pattern.exec(newMes);
+if(newMess){
+
+    let match = pattern.exec(newMess);
 
     if(match.groups.type === "A"){
 
-        attackedPlanetsList.add(match)       
+        attackedPlanetsList.add(match);
 
     }
 
     if(match.groups.type === "D"){
 
-        destroyedPlanetsList.add(match)
+        destroyedPlanetsList.add(match);
+
     }
 
-  }
+    console.log(`Attacked planets: ${attackedPlanetsList.size}`);
 
-console.log(`Attacked planets: ${attackedPlanetsList.size}`)
+    let sortAttacked = attackedPlanetsList.sort((a,b)=> a[0].localeCompare(b[0]));
 
-let sortedAttacked = attackedPlanetsList.sort((a,b)=> a[0].localeCompare(b[0]));
+    for(let el of sortAttacked){
 
-console.table(sortedAttacked)
+        console.log(`-> ${el[0]}`)
 
-//console.log(`-> ${sortedAttacked[0]}`)
+    }
 
+
+
+    console.log(`Destroyed planets: ${destroyedPlanetsList.size}`);
+
+    let sortDestroyed = destroyedPlanetsList.sort((a,b)=> a[0].localeCompare(b[0]));
+
+    for(let el of sortDestroyed){
+
+        console.log(`-> ${el[0]}`)
+
+    }
+}
 }
 
 starEnigma(
     ['3',
         "tt(''DGsvywgerx>6444444444%H%1B9444",
-        //'GQhrr|A977777(H(TTTT',
-        //'EHfsytsnhf?8555&I&2C9555SR'
+        'GQhrr|A977777(H(TTTT',
+        'EHfsytsnhf?8555&I&2C9555SR'
         ]
     )
