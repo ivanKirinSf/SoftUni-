a636dfcea7a452bfeaf10afbf823b1c6018a9ad7
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-03-29 19:15:39 +0200

    Update 04starEnigma.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/04starEnigma.js
@@ -16,73 +16,86 @@ let destroyedPlanetsList = new Set();
 
 for(let line of input){
 
-  let match = starCode.exec(line);
+let match = starCode.exec(line);
 
-  while(match !== null){
+while(match !== null){
 
-    counter ++;
+  counter ++;
 
-   match = starCode.exec(line)
+  match = starCode.exec(line);
 
-  }
-
-  for(let ch of line){
-     
-    let value = ch.charCodeAt();
-
-    let res = value - counter;
-
-    let char = String.fromCharCode(res);
-
-    newMess += char;
-
-  }
 }
 
-if(pattern.test(newMess)){
+for(let ch of line){
 
-  let match = pattern.exec(newMess);
+  let value = ch.charCodeAt();
 
-  if(match.groups.type === "A"){
+  let res = value - counter;
 
-    attackedPlanetsList.add(match.groups.name);
+  let char = String.fromCharCode(res);
 
-  }
+  newMess += char;
 
-  if(match.groups.type === "D"){
-
-    destroyedPlanetsList.add(match.groups.name); 
-
-  }
-}
-
-console.log(`Attacked planets: ${attackedPlanetsList.size}`);
-
-for(let planet in attackedPlanetsList){
-
-  console.log(`-> ${planet}`)
-}
-
-console.log(`Destroyed planets: ${destroyedPlanetsList.size}`);
-
-for(let planet of attackedPlanetsList){
-
-  console.log(`-> ${planet}`)
 }
 
 //console.log(newMess)
 
+
+
+
+if(pattern.test(newMess)){
+
+let match = newMess.match(pattern);
+
+if(match.groups.type === "A"){
+
+  attackedPlanetsList.add(match.groups.name); 
+
+}else if(match.groups.type === "D"){
+
+  destroyedPlanetsList.add(match.groups.name);  
+
 }
 
+newMess = "";
 
+counter = 0;
 
+}
 
+}
+
+console.log(`Attacked planets: ${attackedPlanetsList.size}`);
+
+if(attackedPlanetsList.size){
+
+   for(let planet of Array.from(attackedPlanetsList).sort((a,b) => a.localeCompare(b))){
+
+    console.log(`-> ${planet}`);
+
+   }
+
+}
+
+console.log(`Destroyed planets: ${destroyedPlanetsList.size}`);
+
+if(destroyedPlanetsList.size){
+
+  for(let planet of Array.from(destroyedPlanetsList).sort((a,b) => a.localeCompare(b))){
+
+   console.log(`-> ${planet}`);
+   
+  }
+
+}
+
+}
 
 starEnigma(
-    ['3',
-        "tt(''DGsvywgerx>6444444444%H%1B9444",
-        //'GQhrr|A977777(H(TTTT',
-        //'EHfsytsnhf?8555&I&2C9555SR'
-        ]
-        
+
+  ['3',
+    "tt(''DGsvywgerx>6444444444%H%1B9444",
+    'GQhrr|A977777(H(TTTT',
+    'EHfsytsnhf?8555&I&2C9555SR'
+  ]              
     )
