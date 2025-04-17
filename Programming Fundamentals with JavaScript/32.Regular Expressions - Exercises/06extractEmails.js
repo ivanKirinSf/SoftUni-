d039a85624aefcebd535ddb41ec8b9ae96fd0b66
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-04-17 20:49:11 +0300

    Update 06extractEmails.js

--- Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/06extractEmails.js
+++ Programming Fundamentals with JavaScript/32.Regular Expressions - Exercises/06extractEmails.js
@@ -1,12 +1,12 @@
 function extractEmails(input){
 
 
-    let pattern = /(?<=./-)([A-Za-z0-9]+[A-Za-z0-9./-_]*)@([a-z]+\-)) /g;
+    let pattern = /(?<=s\|^)([A-Za-z0-9]+[A-Za-z0-9./-_])+([a-z]*)+([a-z])/g;
 
-    let mails = input.match(pattern);
+    let mails = pattern.match(input);
 
     mails.forEach(x => console.log(x));
-    
+
 }
 
 extractEmails("Please contact us at: support@github.com. Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.")
