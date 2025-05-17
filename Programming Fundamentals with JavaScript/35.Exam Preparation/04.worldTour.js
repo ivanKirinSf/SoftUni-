221f37c0f33823725d9428ad38917ba19c4fce3e
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-05-17 18:47:04 +0300

    Update 04.worldTour.js

--- Programming Fundamentals with JavaScript/35.Exam Preparation/04.worldTour.js
+++ Programming Fundamentals with JavaScript/35.Exam Preparation/04.worldTour.js
@@ -12,7 +12,7 @@ for(let el of input){
 
         let index = Number(temp.shift());
 
-        if(index >= 0 && index <= str.length){
+        if(index >= 0 && index <= str.length-1){
 
             let string = temp.shift();
 
@@ -23,17 +23,53 @@ for(let el of input){
             str = sub1 + string + sub2;   
         }
         
+        console.log(str);
+
+    }else if(com === "Remove Stop"){
+
+        let startIndex = Number(temp.shift());
+
+        let endIndex = Number(temp.shift());
+
+        if(startIndex >= 0 && endIndex <= str.length-1){
+
+        let sub1 = str.substring(0, startIndex);
+
+        let sub2 = str.substring(endIndex+1, str.length);
+
+        str = sub1+sub2;        
+
+        }  
+        
+        console.log(str);
+        
+    }else if(com === "Switch"){
+
+        let oldString = temp.shift();
+
+        let newString = temp.shift();
+
+        if(str.includes(`${oldString}`)){
+
+        str = str.replace(oldString, newString);        
+
+        }        
+
         console.log(str)
+
+    }else if(com === "Travel"){
+
+        console.log(`Ready for world tour! Planned stops: ${str}`);
+        break;
     }
-}
-    
+}    
 
 }
 
 worldTour([
-    "Hawai::Cyprys-Greece",
-    "Add Stop:20:Rome",
-    //"Remove Stop:11:16",
-    //"Switch:Hawai:Bulgaria",
-    "Travel"
+"Albania:Bulgaria:Cyprus:Deuchland",
+"Add Stop:3:Nigeria",
+"Remove Stop:4:8",
+"Switch:Albania: Azərbaycan",
+"Travel"
 ])
