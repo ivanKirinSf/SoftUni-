4e555a96bf6475c0a5d302b598cb4cf29b6852ff
Ivan Kirin <arch.ivan.kirin@gmail.com>
2025-06-27 21:38:43 +0300

    Update 06thePianist.js

--- Programming Fundamentals with JavaScript/35.Exam Preparation/06thePianist.js
+++ Programming Fundamentals with JavaScript/35.Exam Preparation/06thePianist.js
@@ -22,10 +22,75 @@ let commands = input.shift()
 
 while(commands !== 'Stop'){
 
-  console.log(commands);
+  let tempCom = commands.split("|");
 
+  let command = tempCom.shift();
+
+  if(command === "Add"){
+
+    let pName = tempCom.shift();
+
+    let pComp = tempCom.shift();
+    
+    let pKey = tempCom.shift();
+
+    if( pName in pieces){
+
+       console.log(`${pName} is already in the collection!`)
+
+    }else{
+
+      pieces[pName] = {composer: pComp, key: pKey};
+
+      console.log(`${pName} by ${pComp} in ${pKey} added to the collection!`)
+
+    }
+
+  }else if(command === "Remove"){
+
+     let pName = tempCom.shift();
+
+     if(pName in pieces){
+
+       delete pieces[pName];
+
+       console.log(`Successfully removed ${pName}!`)
+
+     }else {
+
+       console.log(`Invalid operation! ${pName} does not exist in the collection.`)
+
+     }
+
+  }else if(command === "ChangeKey"){
+
+    let pName = tempCom.shift();
+
+    let pKey = tempCom.shift();
+
+    if(pName in pieces){
+
+      pieces[pName].key = pKey; 
+
+      console.log(`Changed the key of ${pName} to ${pKey}!`);
+
+    } else {
+
+      console.log(`Invalid operation! ${pName} does not exist in the collection.`)
+
+    }       
+
+  }
+  //console.log(command)
   commands = input.shift();
 
+}
+
+let entries = Object.entries(pieces);
+
+for(let [piece, info] of entries){
+
+  console.log(`${piece} -> Composer: ${info.composer}, Key: ${info.key}`)
 }
 
 }
@@ -43,4 +108,5 @@ thePianist(
   'ChangeKey|Moonlight Sonata|C# Major',
   'Stop'  
 ]
+
 )
